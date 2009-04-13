70665e018e9e1ceadebd305b0cf24ba46b1c8ee8
sapetnioc <yann@cointepas.net>
2009-04-13 15:30:17 +0000

    Projects aims, aims-gpl, anatomist and share are now using brainvisa-cmake (former soma-infra)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/perforce/soma-infra/trunk@32398 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/modules/FindDCMTK-runtime.cmake
+++ cmake/modules/FindDCMTK-runtime.cmake
@@ -1,6 +1,6 @@
 find_package( DCMTK2 REQUIRED QUIET )
-SOMA_INSTALL_RUNTIME_LIBRARIES( ${DCMTK_LIBRARIES} )
+BRAINVISA_INSTALL_RUNTIME_LIBRARIES( ${DCMTK_LIBRARIES} )
 get_filename_component( _dir "${DCMTK_dcmdata_LIBRARY}" PATH )
 if( EXISTS "${_dir}/dicom.dic" )
-  SOMA_INSTALL( FILES "${_dir}/dicom.dic" DESTINATION lib COMPONENT system-runtime )
+  BRAINVISA_INSTALL( FILES "${_dir}/dicom.dic" DESTINATION lib COMPONENT system-runtime )
 endif( EXISTS "${_dir}/dicom.dic" )

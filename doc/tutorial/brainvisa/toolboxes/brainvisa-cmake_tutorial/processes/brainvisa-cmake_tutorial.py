8203a5211e12a9fbabd74e849ddbfdfa5fe16ac8
sapetnioc <yann@cointepas.net>
2009-04-23 17:09:43 +0000

    Example for brainvisa-cmake usage
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/perforce/soma-infra/trunk@32597 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ doc/tutorial/brainvisa/toolboxes/brainvisa-cmake_tutorial/processes/brainvisa-cmake_tutorial.py
@@ -0,0 +1,10 @@
+
+from neuroProcesses import *
+
+signature = Signature()
+
+def initialization( self ):
+  pass
+  
+def execution( self, context ):
+  context.write( 'Sample process for project brainvisa-cmake_tutorial' )

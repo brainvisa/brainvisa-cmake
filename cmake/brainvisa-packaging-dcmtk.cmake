cf0b12fa053cda90e7ed72ed688984ee3d43954c
sapetnioc <yann@cointepas.net>
2009-11-28 15:58:47 +0000

    #366 Finished infrastructure for the definition of third party software packagers. We can now write brainvisa-packaging-*.cmake file where * is a package name. These files contains CMake code indicating which file are to be packaged. For instance, here are the list of third party packages currently used in BRAINVISA_DEPENDENCY:   libqtcore4   libqtgui4   libqt4-qt3support   python-sip4   python-qt4   libsigc++-2.0-0c2a   libsigc++-2.0   libxml2   libboost   blitz++   python-numpy   sip   libjpeg62   libminc   libtiff   libexpat1   libqt4-opengl   PythonLibs
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@35779 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ cmake/brainvisa-packaging-dcmtk.cmake
@@ -0,0 +1,45 @@
+find_package( DCMTK REQUIRED )
+
+function( BRAINVISA_PACKAGING_COMPONENT_INFO package_name package_maintainer package_version )
+  set( ${package_name} brainvisa-dcmtk PARENT_SCOPE )
+  set( ${package_maintainer} "IFR 49" PARENT_SCOPE )
+  # Find version
+  set( ${package_version} "no_version" PARENT_SCOPE )
+  foreach( include ${DCMTK_INCLUDE_DIR} )
+    if( EXISTS "${include}/dcmtk/dcmdata/dcuid.h" )
+      file( READ "${include}/dcmtk/dcmdata/dcuid.h" header )
+      string( REGEX MATCH "#define[ \\t]*OFFIS_DCMTK_VERSION_STRING[ \\t]*\"([^\"]*)\"" match "${header}" )
+      if( match )
+        set( ${package_version} "${CMAKE_MATCH_1}" PARENT_SCOPE )
+        break()
+      endif()
+    endif()
+  endforeach()
+endfunction()
+
+
+function( BRAINVISA_PACKAGING_COMPONENT_RUN component )
+  BRAINVISA_REAL_PATHS( real ${DCMTK_LIBRARIES} )
+  BRAINVISA_INSTALL( FILES ${real} ${DCMTK_LIBRARIES}
+    DESTINATION "lib"
+    COMPONENT "${component}" )
+endfunction()
+
+
+function( BRAINVISA_PACKAGING_COMPONENT_DEV component )
+  BRAINVISA_INSTALL_DIRECTORY( "${DCMTK_INCLUDE_DIR}"
+                              "include"
+                              "${component}-devel" )
+endfunction()
+
+
+function( BRAINVISA_PACKAGING_COMPONENT_DOC component )
+endfunction()
+
+
+function( BRAINVISA_PACKAGING_COMPONENT_DEVDOC component )
+endfunction()
+
+
+function( BRAINVISA_PACKAGING_COMPONENT_SRC component )
+endfunction()

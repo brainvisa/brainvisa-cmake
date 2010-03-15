7ec0e265942c3978afd3035b0070aaf5ad336b43
sapetnioc <yann@cointepas.net>
2010-03-15 13:20:33 +0000

    #366 New rules to automatically find the appropriate Qwt version taking into account unstandard installations and Qt4/Qt3 versions
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@36937 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/modules/FindQwt.cmake
+++ cmake/modules/FindQwt.cmake
@@ -1,35 +1,41 @@
-IF( QWT_FOUND )
-ELSE ( QWT_FOUND )
+if( NOT QWT_FOUND )
+  set( paths qwt-qt${DESIRED_QT_VERSION} qwt5-qt${DESIRED_QT_VERSION} qwt qwt5 )
+  set( include_paths )
+  set( lib_paths )
+  foreach( path ${paths} )
+    set( include_paths ${include_paths} "${path}" "${path}/include" )
+    set( lib_paths ${lib_paths} "${path}" "${path}/lib" )
+  endforeach()
 
-FIND_PATH( QWT_INCLUDE_DIR qwt.h 
-               PATHS ${QT_INCLUDE_DIR} /usr/local/qwt/include /usr/include/qwt
-               PATH_SUFFIXES qwt qwt5 qwt-qt${DESIRED_QT_VERSION} qwt5-qt${DESIRED_QT_VERSION} include qwt/include qwt5/include qwt-qt${DESIRED_QT_VERSION}/include qwt5-qt${DESIRED_QT_VERSION}/include
-               ENV PATH )
-
-FIND_LIBRARY( QWT_LIBRARY 
-              NAMES qwt5-qt${DESIRED_QT_VERSION} qwt-qt${DESIRED_QT_VERSION} qwt5 qwt
-              PATHS /usr/local /usr
-              PATH_SUFFIXES qwt qwt5 qwt-qt${DESIRED_QT_VERSION} qwt5-qt${DESIRED_QT_VERSION} lib qwt/lib qwt5/lib qwt-qt${DESIRED_QT_VERSION}/lib qwt5-qt${DESIRED_QT_VERSION}/lib
-            )
-
-SET(QWT_FOUND FALSE)
-
-IF(QWT_INCLUDE_DIR AND QWT_LIBRARY)
-
-  SET(QWT_FOUND TRUE)
+  # First look only in paths from CMAKE_PREFIX_PATH
+  find_path( QWT_INCLUDE_DIR qwt.h 
+             PATHS ${CMAKE_PREFIX_PATH}
+             PATH_SUFFIXES ${include_paths}
+             NO_CMAKE_PATH NO_CMAKE_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH )
+  find_library( QWT_LIBRARY 
+                NAMES qwt5-qt${DESIRED_QT_VERSION} qwt-qt${DESIRED_QT_VERSION} qwt5 qwt
+                PATHS ${CMAKE_PREFIX_PATH}
+                PATH_SUFFIXES ${lib_paths}
+                NO_CMAKE_PATH NO_CMAKE_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH )
+  # Then look into all standard paths
+  find_path( QWT_INCLUDE_DIR qwt.h 
+             PATH_SUFFIXES ${include_paths} )
+  find_library( QWT_LIBRARY 
+                NAMES qwt5-qt${DESIRED_QT_VERSION} qwt-qt${DESIRED_QT_VERSION} qwt5 qwt
+                PATH_SUFFIXES ${lib_paths} )
   
-ENDIF(QWT_INCLUDE_DIR AND QWT_LIBRARY)
-
-
-IF(NOT QWT_FOUND)
-  IF(NOT QWT_FIND_QUIETLY)
-    MESSAGE(STATUS "Qwt was not found.")
-  ELSE(NOT QWT_FIND_QUIETLY)
-    IF(QWT_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Qwt was not found.")
-    ENDIF(QWT_FIND_REQUIRED)
-  ENDIF(NOT QWT_FIND_QUIETLY)
-ENDIF(NOT QWT_FOUND)
-
-
-ENDIF( QWT_FOUND )
+  SET(QWT_FOUND FALSE)
+  IF(QWT_INCLUDE_DIR AND QWT_LIBRARY)
+    SET(QWT_FOUND TRUE)
+  ENDIF(QWT_INCLUDE_DIR AND QWT_LIBRARY)
+  
+  IF(NOT QWT_FOUND)
+    IF(NOT QWT_FIND_QUIETLY)
+      MESSAGE(STATUS "Qwt was not found.")
+    ELSE(NOT QWT_FIND_QUIETLY)
+      IF(QWT_FIND_REQUIRED)
+        MESSAGE(FATAL_ERROR "Qwt was not found.")
+      ENDIF(QWT_FIND_REQUIRED)
+    ENDIF(NOT QWT_FIND_QUIETLY)
+  ENDIF(NOT QWT_FOUND)
+endif()

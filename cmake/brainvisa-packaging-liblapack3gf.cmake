9e6e9da2bf64061cf54d88de3ecfbaa53fe68046
dgeffroy <dgeffroy@users.noreply.github.com>
2010-08-27 06:43:01 +0000

    #366 Packaging rules for png12 and lapack
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@38759 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ cmake/brainvisa-packaging-liblapack3gf.cmake
@@ -0,0 +1,30 @@
+enable_language(Fortran)
+find_package(LAPACK REQUIRED)
+
+function( BRAINVISA_PACKAGING_COMPONENT_INFO component package_name package_maintainer package_version )
+  set( ${package_name} brainvisa-${component} PARENT_SCOPE )
+  set( ${package_maintainer} "IFR 49" PARENT_SCOPE )
+  # Find version
+  set( ${package_version} "no_version" PARENT_SCOPE )
+endfunction()
+
+
+function( BRAINVISA_PACKAGING_COMPONENT_RUN component )
+  BRAINVISA_INSTALL_RUNTIME_LIBRARIES( ${component} ${LAPACK_LIBRARIES} )
+endfunction()
+
+
+function( BRAINVISA_PACKAGING_COMPONENT_DEV component )
+endfunction()
+
+
+function( BRAINVISA_PACKAGING_COMPONENT_DOC component )
+endfunction()
+
+
+function( BRAINVISA_PACKAGING_COMPONENT_DEVDOC component )
+endfunction()
+
+
+function( BRAINVISA_PACKAGING_COMPONENT_SRC component )
+endfunction()
---
+++ cmake/brainvisa-packaging-libpng12-0.cmake
@@ -0,0 +1,43 @@
+find_library(PNG12 REQUIRED)
+
+function( BRAINVISA_PACKAGING_COMPONENT_INFO component package_name package_maintainer package_version )
+  set( ${package_name} brainvisa-${component} PARENT_SCOPE )
+  set( ${package_maintainer} "IFR 49" PARENT_SCOPE )
+  # Find version
+  set( ${package_version} "no_version" PARENT_SCOPE )
+  if(PNG12_VERSION)
+    set( ${package_version} "${PNG12_VERSION}" PARENT_SCOPE )
+  else(PNG12_VERSION)
+    if( EXISTS "${PNG12_INCLUDE_DIR}/png.h" )
+      file( READ "${PNG12_INCLUDE_DIR}/png.h" header )
+      string( REGEX MATCH "#define[ \\t]*PNG_LIBPNG_VER_STRING[ \\t]*\"([^\"]*)\"" match "${header}" )
+      if( match )
+        set( ${package_version} "${CMAKE_MATCH_1}" PARENT_SCOPE )
+      endif( match )
+    endif()
+  endif(PNG12_VERSION)
+endfunction()
+
+
+function( BRAINVISA_PACKAGING_COMPONENT_RUN component )
+  BRAINVISA_INSTALL_RUNTIME_LIBRARIES( ${component} ${PNG12_LIBRARIES} )
+endfunction()
+
+
+function( BRAINVISA_PACKAGING_COMPONENT_DEV component )
+  BRAINVISA_INSTALL_DIRECTORY( "${PNG12_INCLUDE_DIR}"
+                              "include"
+                              "${component}-devel" )
+endfunction()
+
+
+function( BRAINVISA_PACKAGING_COMPONENT_DOC component )
+endfunction()
+
+
+function( BRAINVISA_PACKAGING_COMPONENT_DEVDOC component )
+endfunction()
+
+
+function( BRAINVISA_PACKAGING_COMPONENT_SRC component )
+endfunction()

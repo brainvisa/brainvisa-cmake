6f86880aadfabb9f2b40081a68af5307b5023d0b
dgeffroy <dgeffroy@users.noreply.github.com>
2010-09-02 14:25:37 +0000

    #366 Rename FindGCC into FindLIBGCC.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@38855 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/brainvisa-packaging-libgcc1.cmake
+++ cmake/brainvisa-packaging-libgcc1.cmake
@@ -1,11 +1,11 @@
-find_package( GCC ) 
+find_package( LIBGCC ) 
 
 function( BRAINVISA_PACKAGING_COMPONENT_INFO component package_name package_maintainer package_version )
   set( ${package_name} ${component} PARENT_SCOPE )
   set( ${package_maintainer} "IFR 49" PARENT_SCOPE )
   # Find version
   set( ${package_version} "0.0.0" PARENT_SCOPE )
-  get_filename_component( real "${GCC_LIBRARIES}" REALPATH )
+  get_filename_component( real "${LIBGCC_LIBRARIES}" REALPATH )
   string( REGEX MATCH "^.*libgcc_s[-](.*)${CMAKE_SHARED_LIBRARY_SUFFIX}.*$" match "${real}" )
   if( match )
     set( ${package_version} "${CMAKE_MATCH_1}" PARENT_SCOPE )
@@ -14,7 +14,7 @@ endfunction()
 
 
 function( BRAINVISA_PACKAGING_COMPONENT_RUN component )
-  if( GCC_FOUND )
-    BRAINVISA_INSTALL_RUNTIME_LIBRARIES( ${component} ${GCC_LIBRARIES} )
+  if( LIBGCC_FOUND )
+    BRAINVISA_INSTALL_RUNTIME_LIBRARIES( ${component} ${LIBGCC_LIBRARIES} )
   endif()
 endfunction()

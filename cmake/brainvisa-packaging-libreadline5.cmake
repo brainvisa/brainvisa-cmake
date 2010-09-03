c6bb45994141ca2616ca1e1a966f28fc435d6484
sapetnioc <yann@cointepas.net>
2010-09-03 21:03:20 +0000

    #1652 Fixed libreadline5 and libgcc_s searching for Ubuntu
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@38888 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/brainvisa-packaging-libreadline5.cmake
+++ cmake/brainvisa-packaging-libreadline5.cmake
@@ -1,11 +1,19 @@
-find_package( LibReadline ) 
+find_library( LIBREADLINE5 readline )
+if( NOT LIBREADLINE5 )
+  # On Ubuntu 10.4 libreadline is in /lib/libreadline.so.* and CMake cannot find it
+  # because there is no /lib/libreadline.so
+  file( GLOB LIBREADLINE5 /lib/libreadline.so.5 )
+  if( LIBREADLINE5 )
+    set( LIBREADLINE5 "${LIBREADLINE5}" CACHE STRING "libreadline library" FORCE )
+  endif()
+endif()
 
 function( BRAINVISA_PACKAGING_COMPONENT_INFO component package_name package_maintainer package_version )
   set( ${package_name} ${component} PARENT_SCOPE )
   set( ${package_maintainer} "IFR 49" PARENT_SCOPE )
   # Find version
   set( ${package_version} "0.0.0" PARENT_SCOPE )
-  get_filename_component( real "${LIBREADLINE_LIBRARIES}" REALPATH )
+  get_filename_component( real "${LIBREADLINE5}" REALPATH )
   string( REGEX MATCH "^.*libreadline${CMAKE_SHARED_LIBRARY_SUFFIX}[.](.*)$" match "${real}" )
   if( match )
     set( ${package_version} "${CMAKE_MATCH_1}" PARENT_SCOPE )
@@ -14,9 +22,9 @@ endfunction()
 
 
 function( BRAINVISA_PACKAGING_COMPONENT_RUN component )
-  if( LIBREADLINE_FOUND )
-    BRAINVISA_INSTALL_RUNTIME_LIBRARIES( ${component} "${LIBREADLINE_LIBRARIES}" )
+  if( LIBREADLINE5 )
+    BRAINVISA_INSTALL_RUNTIME_LIBRARIES( ${component} "${LIBREADLINE5}" )
   else()
-    MESSAGE( SEND_ERROR "Impossible to create packaging rules for ${component} : the package was not found." )
+    message( SEND_ERROR "Impossible to create packaging rules for ${component} : the library was not found." )
   endif()
 endfunction()
--- cmake/modules/FindLIBGCC.cmake
+++ cmake/modules/FindLIBGCC.cmake
@@ -3,23 +3,29 @@
 # LIBGCC_FOUND
 # LIBGCC_LIBRARIES - the gcc library
 
-IF(LIBGCC_LIBRARIES)
+if( LIBGCC_LIBRARIES )
   # already found  
-  SET(LIBGCC_FOUND TRUE)
-ELSE()
-  FIND_LIBRARY( LIBGCC_LIBRARIES gcc_s )
-  IF(LIBGCC_LIBRARIES)
-    SET(LIBGCC_FOUND TRUE)
-  ELSE()
-    SET(LIBGCC_FOUND FALSE)
+  set( LIBGCC_FOUND TRUE )
+else()
+  find_library( LIBGCC_LIBRARIES gcc_s )
+  if( NOT LIBGCC_LIBRARIES )
+    # On Ubuntu 10.4 libgcc_s is in /lib/libgcc_s.so.1 and CMake cannot find it
+    # because there is no /lib/libgcc_s.so
+    file( GLOB LIBGCC_LIBRARIES /lib/libgcc_s.so.? )
+    if( LIBGCC_LIBRARIES )
+      set( LIBGCC_LIBRARIES "${LIBGCC_LIBRARIES}" CACHE PATH "libgcc_s library" FORCE )
+    endif()
+  endif()
+  if( LIBGCC_LIBRARIES )
+    set( LIBGCC_FOUND TRUE )
+  else()
+    set( LIBGCC_FOUND FALSE )
       
-    IF( LIBGCC_FIND_REQUIRED )
-        MESSAGE( SEND_ERROR "LIBGCC was not found." )
-    ENDIF()
-    IF(NOT LIBGCC_FIND_QUIETLY)
-        MESSAGE(STATUS "LIBGCC was not found.")
-    ENDIF()
-  ENDIF()
-
-ENDIF()
+    if( LIBGCC_FIND_REQUIRED )
+        message( SEND_ERROR "LIBGCC was not found." )
+    elseif( NOT LIBGCC_FIND_QUIETLY )
+        message( STATUS "LIBGCC was not found." )
+    endif()
+  endif()
+endif()
 

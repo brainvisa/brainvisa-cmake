9136c125b6506bff6e86312684665bd10ee78f98
dgeffroy <dgeffroy@users.noreply.github.com>
2010-09-14 07:34:08 +0000

    #366 - Packaging rules for libcairo - python dependency on libcairo because of matplotlib - Use find_library instead of find_package in FindLibSSL, to make libssl-dev package optional : find_package(OpenSSL) search for include files.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@39005 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ cmake/brainvisa-packaging-libcairo2.cmake
@@ -0,0 +1,21 @@
+find_package( LibCairo ) 
+
+function( BRAINVISA_PACKAGING_COMPONENT_INFO component package_name package_maintainer package_version )
+  set( ${package_name} ${component} PARENT_SCOPE )
+  set( ${package_maintainer} "IFR 49" PARENT_SCOPE )
+  # Find version
+  set( ${package_version} "0.0.0" PARENT_SCOPE )
+  get_filename_component( real "${LIBCAIRO_LIBRARIES}" REALPATH )
+  string( REGEX MATCH "^.*libcairo${CMAKE_SHARED_LIBRARY_SUFFIX}[.](.*)$" match "${real}" )
+  if( match )
+    set( ${package_version} "${CMAKE_MATCH_1}" PARENT_SCOPE )
+  endif()
+endfunction()
+
+function( BRAINVISA_PACKAGING_COMPONENT_RUN component )
+  if( LIBCAIRO_FOUND)
+    BRAINVISA_INSTALL_RUNTIME_LIBRARIES( ${component} ${LIBCAIRO_LIBRARIES} )
+  else()
+    MESSAGE( SEND_ERROR "Impossible to create packaging rules for ${component} : the library was not found." )
+  endif()
+endfunction()
---
+++ cmake/modules/FindLibCairo.cmake
@@ -0,0 +1,26 @@
+# Find LibCairo
+#
+# LIBCAIRO_FOUND
+# LIBCAIRO_LIBRARIES - cairo library
+
+IF(LIBCAIRO_LIBRARIES)
+  # already found  
+  SET(LIBCAIRO_FOUND TRUE)
+ELSE()
+  find_library(LIBCAIRO_LIBRARIES cairo)
+  IF(LIBCAIRO_LIBRARIES)
+    set(LIBCAIRO_LIBRARIES ${LIBCAIRO_LIBRARIES} CACHE PATH "LibCairo libraries")
+    SET(LIBCAIRO_FOUND TRUE)
+  ELSE()
+    SET(LIBCAIRO_FOUND FALSE)
+      
+    IF( LIBCAIRO_FIND_REQUIRED )
+        MESSAGE( SEND_ERROR "LibCairo was not found." )
+    ENDIF()
+    IF(NOT LIBCAIRO_FIND_QUIETLY)
+        MESSAGE(STATUS "LibCairo was not found.")
+    ENDIF()
+  ENDIF()
+
+ENDIF()
+

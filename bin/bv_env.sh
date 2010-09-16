acddb743f96f98ccd476fe4b8f24e517f54d91b5
sapetnioc <yann@cointepas.net>
2010-09-16 11:17:04 +0000

    Look into the shell history to find the location of bv_env when user used "source /somewhere/bin/bv_env.sh"
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@39064 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- bin/bv_env.sh
+++ bin/bv_env.sh
@@ -1,17 +1,39 @@
 #! /bin/sh
+
+exists() {
+  type -t $1 > /dev/null
+}
+
 mktemp=`type -t mktemp`
 if [ -z "$mktemp" ]; then
   i=0
-  tmp="/tmp/tmp-$$-$i"
+  tmp="/tmp/bv_env-$$-$i"
   while [ -e "$tmp" ]; do
     i=$(($i+1))
-    tmp="/tmp/tmp-$$-$i"
+    tmp="/tmp/bv_env-$$-$i"
   done
-  echo 'self: tmp='$tmp
 else
   tmp=`mktemp -t bv_env`
-  echo 'mktemp: tmp='$tmp
 fi
-bv_env > "$tmp"
+
+if [ -e "$0" ]; then
+  bv_env="`dirname "$0"`"
+  if [ -z "$bv_env" -o "$bv_env" = "." ]; then
+    bv_env="$PWD/bv_env"
+  else
+    bv_env="$bv_env/bv_env"
+  fi
+else
+  bv_env=bv_env
+  # Try to find bv_env location in the shell history
+  exists history && exists tail && exists python
+  if [ $? -eq 0 ]; then
+    bv_env="`history | tail -n 1 | python -c 'import sys,os; print os.path.abspath( os.path.join( os.path.dirname( sys.stdin.readline().split( None, 3 )[ 2 ] ),"bv_env" ) )' 2>/dev/null`"
+    if [ ! -x "$bv_env" ]; then
+      bv_env=bv_env
+    fi
+  fi
+fi
+"$bv_env" > "$tmp"
 source "$tmp"
 \rm "$tmp"
--- bin/bv_unenv.sh
+++ bin/bv_unenv.sh
@@ -1,17 +1,39 @@
 #! /bin/sh
+
+exists() {
+  type -t $1 > /dev/null
+}
+
 mktemp=`type -t mktemp`
 if [ -z "$mktemp" ]; then
   i=0
-  tmp="/tmp/tmp-$$-$i"
+  tmp="/tmp/bv_env-$$-$i"
   while [ -e "$tmp" ]; do
     i=$(($i+1))
-    tmp="/tmp/tmp-$$-$i"
+    tmp="/tmp/bv_env-$$-$i"
   done
-  echo 'self: tmp='$tmp
 else
-  tmp=`mktemp -t bv_unenv`
-  echo 'mktemp: tmp='$tmp
+  tmp=`mktemp -t bv_env`
 fi
-bv_unenv > "$tmp"
+
+if [ -e "$0" ]; then
+  bv_unenv="`dirname "$0"`"
+  if [ -z "$bv_env" -o "$bv_env" = "." ]; then
+    bv_unenv="$PWD/bv_unenv"
+  else
+    bv_unenv="$bv_env/bv_unenv"
+  fi
+else
+  bv_unenv=bv_unenv
+  # Try to find bv_env location in the shell history
+  exists history && exists tail && exists python
+  if [ $? -eq 0 ]; then
+    bv_unenv="`history | tail -n 1 | python -c 'import sys,os; print os.path.abspath( os.path.join( os.path.dirname( sys.stdin.readline().split( None, 3 )[ 2 ] ),"bv_unenv" ) )' 2>/dev/null`"
+    if [ ! -x "$bv_unenv" ]; then
+      bv_unenv=bv_env
+    fi
+  fi
+fi
+"$bv_unenv" > "$tmp"
 source "$tmp"
 \rm "$tmp"

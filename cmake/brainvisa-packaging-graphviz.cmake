1722f8fde5c43776775d8bea61183b3f0323fc63
dgeffroy <dgeffroy@users.noreply.github.com>
2010-09-21 11:59:44 +0000

    Packaging of graphviz dot executable + dynamic libraries #366
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@39113 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ cmake/brainvisa-packaging-graphviz.cmake
@@ -0,0 +1,38 @@
+find_package( Dot )
+
+function( BRAINVISA_PACKAGING_COMPONENT_INFO cmake package_name package_maintainer package_version )
+  set( ${package_name} ${component} PARENT_SCOPE )
+  set( ${package_maintainer} "IFR 49" PARENT_SCOPE )
+  if( DOT_VERSION )
+    set( ${package_version} "${DOT_VERSION}" PARENT_SCOPE )
+  else()
+    set( ${package_version} "0.0.0" PARENT_SCOPE )
+  endif()
+  BRAINVISA_THIRDPARTY_DEPENDENCY( "${component}" RUN DEPENDS libcairo2 RUN )
+  BRAINVISA_THIRDPARTY_DEPENDENCY( "${component}" RUN DEPENDS libexpat1 RUN )
+  BRAINVISA_THIRDPARTY_DEPENDENCY( "${component}" RUN DEPENDS libfontconfig1 RUN )
+endfunction()
+
+
+function( BRAINVISA_PACKAGING_COMPONENT_RUN component )
+  if( DOT_EXECUTABLE )
+    BRAINVISA_INSTALL( FILES "${DOT_EXECUTABLE}"
+      DESTINATION "bin"
+      COMPONENT "${component}"
+      PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE )
+  endif()
+  if( DOT_LIBRARIES )
+    BRAINVISA_INSTALL_RUNTIME_LIBRARIES( ${component} ${DOT_LIBRARIES} )
+  endif()
+  # Plugins
+  foreach(lib ${DOT_LIBRARIES})
+    get_filename_component(libdir ${lib} PATH)
+    if( EXISTS "${libdir}/graphviz" )
+      BRAINVISA_INSTALL_DIRECTORY( "${libdir}/graphviz"
+                          "lib/graphviz"
+                          "${component}" )
+      break()
+    endif()
+  endforeach()
+endfunction()
+
--- cmake/modules/FindDot.cmake
+++ cmake/modules/FindDot.cmake
@@ -1,3 +1,11 @@
+# Find dot executable and libraries
+# The following variables are set:
+#
+#  DOT_FOUND - Was dot found
+#  DOT_EXECUTABLE  - path to the dot executable
+#  DOT_LIBRARIES - path to graphviz dynamic libraries needed to run dot
+#  DOT_VERSION - dot version
+
 if( DOT_EXECUTABLE )
 
   set( Dot_FOUND true )
@@ -17,11 +25,74 @@ else( DOT_EXECUTABLE )
   
   if( DOT_EXECUTABLE )
     set( DOT_FOUND true )
-    execute_process( COMMAND ${EPYDOC_EXECUTABLE} --version
-                     OUTPUT_VARIABLE _output
-                     RESULT_VARIABLE _result )
+    execute_process( COMMAND "${DOT_EXECUTABLE}" "-V"
+                     OUTPUT_VARIABLE _output  ERROR_VARIABLE _output OUTPUT_STRIP_TRAILING_WHITESPACE
+		     RESULT_VARIABLE _result)
+    # VERSION
+    if( _output AND _result EQUAL 0 )
+      set( _versionRegex ".*version ([0-9]*)\\.([0-9]*)\\.([0-9]*).*" )
+      string( REGEX REPLACE "${_versionRegex}" "\\1" DOT_VERSION_MAJOR "${_output}" )
+      string( REGEX REPLACE "${_versionRegex}" "\\2" DOT_VERSION_MINOR "${_output}" )
+      string( REGEX REPLACE "${_versionRegex}" "\\3" DOT_VERSION_PATCH "${_output}" )
+      set( DOT_VERSION "${DOT_VERSION_MAJOR}.${DOT_VERSION_MINOR}.${DOT_VERSION_PATCH}")
+      set( DOT_VERSION "${DOT_VERSION}" CACHE STRING "Dot full version")
+    else()
+      message( STATUS "Dot version not found" )
+    endif()
+
+    # LIBRARIES
+    set( DOT_LIBRARIES )
+    find_library( DOT_CDT_LIB cdt )
+    if( NOT DOT_CDT_LIB )
+      file( GLOB DOT_CDT_LIB /usr/lib/libcdt.so.? )
+    endif()
+    if(DOT_CDT_LIB)
+      set( DOT_LIBRARIES ${DOT_LIBRARIES} "${DOT_CDT_LIB}")
+    else()
+      message( STATUS "Graphviz Cdt library not found" )
+    endif()
+    unset(DOT_CDT_LIB CACHE)
+
+    find_library( DOT_GVC_LIB gvc )
+    if( NOT DOT_GVC_LIB )
+      file( GLOB DOT_GVC_LIB /usr/lib/libgvc.so.? )
+    endif()
+    if( DOT_GVC_LIB )
+      set( DOT_LIBRARIES ${DOT_LIBRARIES} "${DOT_GVC_LIB}")
+    else()
+      message( STATUS "Graphviz gvc library not found" )
+    endif()
+    unset(DOT_GVC_LIB CACHE)
+
+    find_library( DOT_PATHPLAN_LIB pathplan )
+    if( NOT DOT_PATHPLAN_LIB )
+      file( GLOB DOT_PATHPLAN_LIB /usr/lib/libpathplan.so.? )
+    endif()
+    if( DOT_PATHPLAN_LIB )
+      set( DOT_LIBRARIES ${DOT_LIBRARIES} "${DOT_PATHPLAN_LIB}")
+    else()
+      message( STATUS "Graphviz pathplan library not found" )
+    endif()
+    unset(DOT_PATHPLAN_LIB CACHE)
+
+    find_library( DOT_GRAPH_LIB graph NO_CMAKE_PATH )
+    message("graph_lib : ${DOT_GRAPH_LIB}")
+    if( NOT DOT_GRAPH_LIB )
+      file( GLOB DOT_GRAPH_LIB /usr/lib/libgraph.so.? )
+    endif()
+    if( DOT_GRAPH_LIB )
+      set( DOT_LIBRARIES ${DOT_LIBRARIES} "${DOT_GRAPH_LIB}")
+    else()
+      message( STATUS "Graphviz graph library not found" )
+    endif()
+    unset(DOT_GRAPH_LIB CACHE)
+
+    if( DOT_LIBRARIES )
+      set( DOT_LIBRARIES ${DOT_LIBRARIES} CACHE PATH "Graphviz dynamic libraries needed to run dot")
+    endif()
+
     if( NOT Dot_FIND_QUIETLY )
-      message( STATUS "Found dot: \"${DOT_EXECUTABLE}\"" )
+      message( STATUS "Found dot: \"${DOT_EXECUTABLE}\" ${DOT_VERSION}" )
     endif( NOT Dot_FIND_QUIETLY )
   else( DOT_EXECUTABLE )
     set( Dot_FOUND false )

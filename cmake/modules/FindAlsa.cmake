24504e9f4992693436841b8bb3ad37fff0ffe328
dgeffroy <dgeffroy@users.noreply.github.com>
2010-09-24 15:01:12 +0000

    Fixes in comment #366
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@39174 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/modules/FindAlsa.cmake
+++ cmake/modules/FindAlsa.cmake
@@ -1,18 +1,20 @@
-# Find dot executable and libraries
+# Find Alsa asound library and include directory
 # The following variables are set:
 #
-#  ALSA_FOUND - Was dot found
+#  ALSA_FOUND - Was alsa found
 #  ALSA_LIBRARIES - asound dynamic library
+#  ALSA_INCLUDE_DIR - include directory where to find alsa/asoundlib.h
 
 if( ALSA_LIBRARIES )
 
   set( ALSA_FOUND true )
 
 else()
-    
+  FIND_PATH( ALSA_INCLUDE_DIR alsa/libasound.h )
+
   find_library( ALSA_LIBRARIES asound )
 
-  IF(ALSA_LIBRARIES)
+  IF(ALSA_INCLUDE_DIR AND ALSA_LIBRARIES)
     SET(ALSA_FOUND TRUE)
   ELSE()
     SET(ALSA_FOUND FALSE)

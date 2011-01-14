bcf0fd791f1b59d8783221a614703de3c4c891e3
sapetnioc <yann@cointepas.net>
2011-01-14 13:46:39 +0000

    Closes #2019 A component is not configured nor compiled by bv_maker if its source directory contains a file named broken_component.log
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@40531 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/brainvisa-compilation.cmake
+++ cmake/brainvisa-compilation.cmake
@@ -15,19 +15,23 @@ BRAINVISA_CREATE_MAIN_COMPONENTS()
 foreach( component ${BRAINVISA_COMPONENTS} )
   if( BRAINVISA_SOURCES_${component} )
     set( ${component}_IS_BEING_COMPILED TRUE CACHE BOOL INTERNAL )
-    message( STATUS "Configuring component ${component} from source directory \"${BRAINVISA_SOURCES_${component}}\"" )
-    if( component STREQUAL brainvisa-cmake )
-      file( MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/build_files/${component}" )
-      execute_process( COMMAND "${CMAKE_COMMAND}" "-DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}" "${BRAINVISA_SOURCES_${component}}"
-        WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/build_files/${component}"
-        OUTPUT_QUIET 
-        ERROR_QUIET )
-      execute_process( COMMAND "${CMAKE_BUILD_TOOL}" install
-        WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/build_files/${component}"
-        OUTPUT_QUIET 
-        ERROR_QUIET )
+    if( EXISTS "${BRAINVISA_SOURCES_${component}}/broken_component.log" )
+      message( "WARNING: Component ${component} is ignored because its compilation was not possible. When the problem is fixed, the component can be reactivated by removing \"${BRAINVISA_SOURCES_${component}}/broken_component.log\"" )
     else()
-      add_subdirectory( "${BRAINVISA_SOURCES_${component}}" "build_files/${component}" )
+      message( STATUS "Configuring component ${component} from source directory \"${BRAINVISA_SOURCES_${component}}\"" )
+      if( component STREQUAL brainvisa-cmake )
+        file( MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/build_files/${component}" )
+        execute_process( COMMAND "${CMAKE_COMMAND}" "-DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}" "${BRAINVISA_SOURCES_${component}}"
+          WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/build_files/${component}"
+          OUTPUT_QUIET 
+          ERROR_QUIET )
+        execute_process( COMMAND "${CMAKE_BUILD_TOOL}" install
+          WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/build_files/${component}"
+          OUTPUT_QUIET 
+          ERROR_QUIET )
+      else()
+        add_subdirectory( "${BRAINVISA_SOURCES_${component}}" "build_files/${component}" )
+      endif()
     endif()
   endif()
 endforeach()

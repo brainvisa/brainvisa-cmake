7e7396204632731598eaccb5c35b3f74652d315b
denisri <denis.riviere.gm@gmail.com>
2011-02-18 17:54:41 +0000

    removed debug message
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@41083 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/modules/FindSphinx.cmake
+++ cmake/modules/FindSphinx.cmake
@@ -20,7 +20,6 @@ else()
   execute_process( COMMAND ${PYTHON_EXECUTABLE} -c "import sphinx; ver = [ int(x) for x in sphinx.__version__.split( '.' ) ]; print '%x' % ( ver[0] * 0x10000 + ver[1] * 0x100 + ver[2] )"
     OUTPUT_VARIABLE SPHINX_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE )
   set( SPHINX_VERSION "${SPHINX_VERSION}" CACHE STRING "Version of sphinx module" )
-  message( "SPHINX_VERSION: ${SPHINX_VERSION}" )
   mark_as_advanced( SPHINX_VERSION )
   set( SPHINX_FOUND TRUE )
 #   if( NOT SPHINX_FIND_QUIETLY )

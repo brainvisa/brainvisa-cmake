8a6d3053861f4fd4d817cd574e5977c2fe551f38
dgeffroy <dgeffroy@users.noreply.github.com>
2011-02-21 09:59:00 +0000

    Improvements in FindSVM #366
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@41090 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/modules/FindSVM.cmake
+++ cmake/modules/FindSVM.cmake
@@ -5,26 +5,37 @@
 # SVM_INCLUDE_DIRS - directory where the header file can be found
 # SVM_LIBRARIES    - the svm libraries
 
-find_path( SVM_INCLUDE_DIRS "svm.h"
-    ${SVM_DIR}/include
-    /usr/local/include
-    /usr/include
-    PATH_SUFFIXES libsvm/include include/libsvm-2.0/libsvm include )
+if(SVM_INCLUDE_DIRS AND SVM_LIBRARIES)
+  set(SVM_FOUND TRUE)
+else()
+  find_path( SVM_INCLUDE_DIRS "svm.h"
+      PATHS ${SVM_DIR}/include
+      /usr/local/include
+      /usr/include
+      PATH_SUFFIXES libsvm/include include/libsvm-2.0/libsvm include )
+  
+  FIND_LIBRARY( SVM_LIBRARIES svm
+    PATHS ${SVM_DIR}/lib
+    /usr/local/lib
+    /usr/lib
+    PATH_SUFFIXES libsvm/lib lib
+  )
+  
+  if( SVM_INCLUDE_DIRS AND SVM_LIBRARIES )
+    set( SVM_FOUND TRUE )
+  else()
+    IF( NOT SVM_FOUND )
+      SET( SVM_DIR "" CACHE PATH "Root of SVM source tree (optional)." )
+      MARK_AS_ADVANCED( SVM_DIR )
+    ENDIF( NOT SVM_FOUND )
+    if( SVM_FIND_REQUIRED )
+        message( SEND_ERROR "SVM library was not found." )
+    else()
+      if(NOT SVM_FIND_QUIETLY)
+        message(STATUS "SVM library was not found.")
+      endif()
+    endif()
 
-FIND_LIBRARY( SVM_LIBRARIES svm
-  ${SVM_DIR}/lib
-  /usr/local/lib
-  /usr/lib
-  PATH_SUFFIXES libsvm/lib lib
-)
-
-IF( SVM_INCLUDE_DIRS AND SVM_LIBRARIES )
-
-  SET( SVM_FOUND TRUE )
-
-ENDIF()
-
-IF( NOT SVM_FOUND )
-  SET( SVM_DIR "" CACHE PATH "Root of SVM source tree (optional)." )
-  MARK_AS_ADVANCED( SVM_DIR )
-ENDIF( NOT SVM_FOUND )
+  endif()
+  
+endif()

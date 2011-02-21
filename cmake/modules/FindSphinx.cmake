d8aac59722ad3ec6bd1e2e79416566292d5469dd
dgeffroy <dgeffroy@users.noreply.github.com>
2011-02-21 09:04:37 +0000

    Fix FindSphinx cmake module created in r41076 Set Sphinx_found to false if it is not found. #1790
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@41088 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/modules/FindSphinx.cmake
+++ cmake/modules/FindSphinx.cmake
@@ -17,13 +17,25 @@ else()
   find_package( python REQUIRED )
 
   mark_as_advanced( SPHINXBUILD_EXECUTABLE )
-  execute_process( COMMAND ${PYTHON_EXECUTABLE} -c "import sphinx; ver = [ int(x) for x in sphinx.__version__.split( '.' ) ]; print '%x' % ( ver[0] * 0x10000 + ver[1] * 0x100 + ver[2] )"
+  if(SPHINXBUILD_EXECUTABLE)
+    execute_process( COMMAND ${PYTHON_EXECUTABLE} -c "import sphinx; ver = [ int(x) for x in sphinx.__version__.split( '.' ) ]; print '%x' % ( ver[0] * 0x10000 + ver[1] * 0x100 + ver[2] )"
     OUTPUT_VARIABLE SPHINX_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE )
-  set( SPHINX_VERSION "${SPHINX_VERSION}" CACHE STRING "Version of sphinx module" )
-  mark_as_advanced( SPHINX_VERSION )
-  set( SPHINX_FOUND TRUE )
-#   if( NOT SPHINX_FIND_QUIETLY )
-#     message( STATUS "Found Sphinx version: ${SPHINX_VERSION}" )
-#   endif(NOT SPHINX_FIND_QUIETLY)
+    set( SPHINX_VERSION "${SPHINX_VERSION}" CACHE STRING "Version of sphinx module" )
+    mark_as_advanced( SPHINX_VERSION )
+  endif()
+  
+  if(SPHINXBUILD_EXECUTABLE AND SPHINX_VERSION)
+    set( SPHINX_FOUND TRUE )
+  else()
+    set(SPHINX_FOUND FALSE)
+    if( SPHINX_FIND_REQUIRED )
+        message( SEND_ERROR "Sphinx was not found." )
+    else()
+      if(NOT _FIND_QUIETLY)
+        message(STATUS "Sphinx was not found.")
+      endif()
+    endif()
+
+  endif()
 endif()
 

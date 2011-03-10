87bcb4f7bb7ae21a27b1f8b52cf255ba621d38ad
denisri <denis.riviere.gm@gmail.com>
2011-03-10 13:05:26 +0000

    added nuclear_imaging project in bioproj_parser
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@41479 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/maker/bioproj_parser.py
+++ python/brainvisa/maker/bioproj_parser.py
@@ -9,9 +9,9 @@
 #
 # This software is governed by the CeCILL-B license under
 # French law and abiding by the rules of distribution of free software.
-# You can  use, modify and/or redistribute the software under the 
+# You can  use, modify and/or redistribute the software under the
 # terms of the CeCILL-B license as circulated by CEA, CNRS
-# and INRIA at the following URL "http://www.cecill.info". 
+# and INRIA at the following URL "http://www.cecill.info".
 #
 # As a counterpart to the access to the source code and  rights to copy,
 # modify and redistribute granted by the license, users are provided only
@@ -26,8 +26,8 @@
 # therefore means  that it is reserved for developers  and  experienced
 # professionals having in-depth computer knowledge. Users are therefore
 # encouraged to load and test the software's suitability as regards their
-# requirements in conditions enabling the security of their systems and/or 
-# data to be ensured and,  more generally, to use and operate it in the 
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
 # same conditions as regards security.
 #
 # The fact that you are presently reading this means that you have had
@@ -41,15 +41,15 @@ BRAINVISA_SVN_URL = SVN_URL + '/brainvisa'
 # Projects that are listed here to define an order of processing
 # for their components. Projects not listed but present in BioProj
 # will be added at the end of this list in an unknown order.
-projects = [ 
-  'communication', 
+projects = [
+  'communication',
   'development',
   'brainvisa-installer',
   'brainvisa-share',
   'soma',
-  'aims', 
-  'anatomist', 
-  'axon', 
+  'aims',
+  'anatomist',
+  'axon',
   'axon_web',
   'data_storage_client',
   'datamind',
@@ -58,8 +58,9 @@ projects = [
   'connectomist',
   'cortical_surface',
   'brainrat',
-  'optical_imaging',
+  'nuclear_imaging',
   'nuclear_processing',
+  'optical_imaging',
   'pyhrf',
   'fmri',
   'famis',
@@ -82,9 +83,10 @@ componentsPerProject = {
  'connectomist': [ 'connectomist-private', 'connectomist-gpl' ],
  'cortical_surface': [ 'cortical_surface-private', 'cortical_surface-gpl' ],
  'fmri': [ 'fmri-private', 'fmri-gpl' ],
- 'optical_imaging' : ['optical_imaging-private', 'optical_imaging-gpl'],
+ 'nuclear_imaging': [ 'nuclear_imaging-gpl', ],
  'nuclear_processing': [ 'nuclear_processing-gpl',
                          'nuclear_processing-private' ],
+ 'optical_imaging' : ['optical_imaging-private', 'optical_imaging-gpl'],
  'soma': [ 'soma-base',  'soma-io', 'soma-qtgui' ],
  'sulci': [ 'sulci-private', 'sulci-gpl', 'sulci-data' ],
  't1mri': [ 't1mri-private', 't1mri-gpl' ],
@@ -94,7 +96,7 @@ componentsPerProject = {
 
 
 groupsDefinition = {
-  'standard': [ 
+  'standard': [
     'aims/*',
     'brainvisa-share/*',
     'anatomist/*',
@@ -109,8 +111,9 @@ groupsDefinition = {
     'development/*',
     'fmri/*',
     'brainvisa-installer/*',
-    'optical_imaging/*',
+    'nuclear_imaging/*',
     'nuclear_processing/*',
+    'optical_imaging/*',
     'soma/*',
     'sulci/*',
     't1mri/*',
@@ -131,8 +134,9 @@ groupsDefinition = {
     'development/*',
     'fmri/*-gpl',
     'brainvisa-installer/*',
-    'optical_imaging/*-gpl',
+    'nuclear_imaging/*-gpl',
     'nuclear_processing/*-gpl',
+    'optical_imaging/*-gpl',
     'soma/*',
     'sulci/*-gpl',
     't1mri/*-gpl',
@@ -174,7 +178,7 @@ def find_branches_and_tags( projects=None, components=None, excludeProjects=excl
     project is the project name (e.g. 'anatomist', 'axon').
     component is the component name (e.g. 'anatomist-gpl', 'anatomist-free') or
       None when the project is a component (such as axon).
-    url is the base Subversion URL for the component (e.g. 
+    url is the base Subversion URL for the component (e.g.
       'https://bioproj.extra.cea.fr/neurosvn/brainvisa/anatomist/anatomist-gpl').
     branches is a list of existing branches sorted from the lowest to the highest
       (e.g. ['3.1', '3.2', '3.12', '3.113']).

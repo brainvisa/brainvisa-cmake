94170a80afcd475c9a38a2cccb0e52b928d26a07
denisri <denis.riviere.gm@gmail.com>
2011-03-14 17:46:29 +0000

    add paths for netcdf and hdf5 in mac-specific
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@41547 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/specific/macos.cmake
+++ cmake/specific/macos.cmake
@@ -1,3 +1,5 @@
 if( APPLE )
-  set( CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "/usr/local/jpeg" "/usr/local/tiff" "/usr/local/sqlite3" )
+  set( CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "/usr/local/jpeg"
+    "/usr/local/tiff" "/usr/local/sqlite3" "/usr/local/netcdf"
+    "/usr/local/hdf5" )
 endif()

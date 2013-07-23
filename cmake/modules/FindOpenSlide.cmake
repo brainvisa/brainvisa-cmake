a3f1e325a3990d48e50df1dbd94684cc63c1104a
denisri <denis.riviere.gm@gmail.com>
2013-07-23 10:33:43 +0000

    make PkgConfig optional
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@57326 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/modules/FindOpenSlide.cmake
+++ cmake/modules/FindOpenSlide.cmake
@@ -7,20 +7,23 @@
 # OPENSLIDE_INCLUDE_DIRS - the OpenSlide include directory
 # OPENSLIDE_LIBRARIES - link to these to use OpenSlide
 
-find_package(PkgConfig REQUIRED)
-pkg_search_module(OPENSLIDE openslide)
-unset(_result)
-unset(_lib)
-unset(_libpath)
-if(OPENSLIDE_FOUND)
-  foreach(_lib ${OPENSLIDE_LIBRARIES})
-    find_library(_libpath ${_lib} ${OPENSLIDE_LIBRARY_DIRS} NO_DEFAULT_PATH)
-    set(_result ${_result} ${_libpath})
-  endforeach()
-
-  set(OPENSLIDE_LIBRARIES ${_result})
-
+find_package(PkgConfig)
+if(PkgConfig_FOUND) # OpenSlide search is supported only through pkg_config.
+  pkg_search_module(OPENSLIDE openslide)
   unset(_result)
   unset(_lib)
   unset(_libpath)
+  if(OPENSLIDE_FOUND)
+    foreach(_lib ${OPENSLIDE_LIBRARIES})
+      find_library(_libpath ${_lib} ${OPENSLIDE_LIBRARY_DIRS} NO_DEFAULT_PATH)
+      set(_result ${_result} ${_libpath})
+    endforeach()
+
+    set(OPENSLIDE_LIBRARIES ${_result})
+
+    unset(_result)
+    unset(_lib)
+    unset(_libpath)
+  endif()
 endif()
+

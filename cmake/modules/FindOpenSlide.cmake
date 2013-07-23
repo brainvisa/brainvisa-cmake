926cf8aac0a3eae84fe2c85ef603d54c959bb7c9
balbasty <balbasty@users.noreply.github.com>
2013-07-23 12:56:05 +0000

    fixes: #7940 - Store some variables in cache.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@57332 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/modules/FindOpenSlide.cmake
+++ cmake/modules/FindOpenSlide.cmake
@@ -6,24 +6,20 @@
 # OPENSLIDE_FOUND - system has OpenSlide
 # OPENSLIDE_INCLUDE_DIRS - the OpenSlide include directory
 # OPENSLIDE_LIBRARIES - link to these to use OpenSlide
+# OPENSLIDE_VERSION - version of OpenSlide used
 
 find_package(PkgConfig)
 if(PkgConfig_FOUND) # OpenSlide search is supported only through pkg_config.
-  pkg_search_module(OPENSLIDE openslide)
-  unset(_result)
-  unset(_lib)
-  unset(_libpath)
-  if(OPENSLIDE_FOUND)
-    foreach(_lib ${OPENSLIDE_LIBRARIES})
-      find_library(_libpath ${_lib} ${OPENSLIDE_LIBRARY_DIRS} NO_DEFAULT_PATH)
-      set(_result ${_result} ${_libpath})
-    endforeach()
+  pkg_search_module(_OPENSLIDE openslide)
+  if(_OPENSLIDE_FOUND)
+    find_library( OPENSLIDE_LIBRARIES ${_OPENSLIDE_LIBRARIES}
+                  PATHS ${_OPENSLIDE_LIBRARY_DIRS} )
+    set( OPENSLIDE_INCLUDE_DIRS ${_OPENSLIDE_INCLUDE_DIRS} CACHE PATH "paths to OpenSlide header files" )
+    set( OPENSLIDE_VERSION "${_OPENSLIDE_VERSION}" CACHE STRING "version of OpenSlide library")
 
-    set(OPENSLIDE_LIBRARIES ${_result})
-
-    unset(_result)
-    unset(_lib)
-    unset(_libpath)
+    if( OPENSLIDE_INCLUDE_DIRS AND OPENSLIDE_LIBRARIES )
+      set( OPENSLIDE_FOUND TRUE CACHE BOOL "specify that OpenSlide library was found")
+    endif()
   endif()
 endif()
 

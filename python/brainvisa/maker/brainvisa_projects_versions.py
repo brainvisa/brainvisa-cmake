1aed798a8b97994cdd68dfd9af38b34eb548c926
denisri <denis.riviere.gm@gmail.com>
2013-12-11 22:50:00 +0000

    update
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/branches/1.2@59966 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/maker/brainvisa_projects_versions.py
+++ python/brainvisa/maker/brainvisa_projects_versions.py
@@ -14,7 +14,7 @@ def project_description( projectname ):
     descriptions = {
         'axon' : 'Axon: organizes processing, pipelining, and data management for neuroimaging. It works both as a graphical user interface or batch and programming interfaces, and allows transparent processing distribution on a computing resource.',
         'aims' : '3D/4D neuroimaging data manipulation and processing library and commands. Includes C++ libraries, command lines, and a Python API.',
-        'anatomist' : '3D/4D neuroimaging data viewer. Modular and versatile, Anatomist can display any kind of neuroimaging data (3D/4D images, meshes and textures, fiber tracts, and structured sets of objects such as sulci), in an arbitrary number of views. Allows C++ and Python programming, both for plugins add-ons, as well as complete custom graphical applications design.',
+        'anatomist' : '3D/4D neuroimaging data viewer. Modular and versatile, Anatomist can display any kind of neuroimaging data (3D/4D images, meshes and textures, fiber tracts, and structured sets of objects such as cortical sulci), in an arbitrary number of views. Allows C++ and Python programming, both for plugins add-ons, as well as complete custom graphical applications design.',
     }
     return descriptions.get( projectname, '' )
 

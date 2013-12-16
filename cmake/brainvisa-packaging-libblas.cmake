7ed0d4e908c31c89ed88f6a77e157fd8b5d4ebe8
nsouedet <nicolas.souedet@gmail.com>
2013-12-16 15:53:03 +0000

    fixes: #9025 - Configuration of blas component fails on Ubuntu 10.04 LTS
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@60090 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/brainvisa-packaging-libblas.cmake
+++ cmake/brainvisa-packaging-libblas.cmake
@@ -15,7 +15,9 @@ function( BRAINVISA_PACKAGING_COMPONENT_INFO component package_name package_main
   endforeach()
   string( REGEX MATCH Ubuntu _systemname "${BRAINVISA_SYSTEM_IDENTIFICATION}" )
   if( _systemname )
-    BRAINVISA_THIRDPARTY_DEPENDENCY( "${component}" RUN DEPENDS libquadmath RUN )
+    if ( "${BRAINVISA_SYSTEM_VERSION}" VERSION_GREATER "12.0")
+      BRAINVISA_THIRDPARTY_DEPENDENCY( "${component}" RUN DEPENDS libquadmath RUN )
+    endif()
   endif()
 endfunction()
 

54072140e5ac07d200ed4424bd26b55f1b8b7373
sapetnioc <yann@cointepas.net>
2015-01-31 16:45:09 +0000

    #11863: For pure Python components, automatically generates <build dir>/build_files/<component>_src/CMakeLists.txt from a template. To date the template is in the pure_python.py module. This modification reactivate Capsul documentation and tests with bv_maker. Installation rules for packaging are not done.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@65476 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/maker/build_models/pure_python.py
+++ python/brainvisa/maker/build_models/pure_python.py
@@ -1,13 +1,43 @@
+import os
 import os.path as osp
+
+cmake_template = '''
+cmake_minimum_required( VERSION 2.6 )
+
+find_package( brainvisa-cmake REQUIRED )
+SET( BRAINVISA_REAL_SOURCE_DIR "%(source_directory)s")
+BRAINVISA_PROJECT()
+
+BRAINVISA_DEPENDENCY( RUN DEPENDS python RUN ">= 2.5;<< 3.0" )
+#BRAINVISA_COPY_PYTHON_DIRECTORY( "%(source_directory)s/%(component_name)s"
+#                                  ${PROJECT_NAME} python/%(component_name)s )
+find_package( python REQUIRED )
+find_package( Sphinx )
+
+BRAINVISA_GENERATE_SPHINX_DOC( "%(source_directory)s/doc/source"
+    "share/doc/capsul-${BRAINVISA_PACKAGE_VERSION_MAJOR}.${BRAINVISA_PACKAGE_VERSION_MINOR}" )
+
+set( BV_ENV_PYTHON_CMD 
+     "${CMAKE_BINARY_DIR}/bin/bv_env" "${PYTHON_EXECUTABLE}" )
+
+# tests
+enable_testing()
+add_test( %(component_name)s-tests "${CMAKE_BINARY_DIR}/bin/bv_env" "${PYTHON_EXECUTABLE}" "%(source_directory)s/test/test_%(component_name)s.py" )
+UNSET( BRAINVISA_REAL_SOURCE_DIR)
+'''
 
 class PurePythonComponentBuild(object):
     def __init__(self, component_name, source_directory, build_directory):
         self.component_name = component_name
         self.source_directory = source_directory
         self.build_directory = build_directory
-        self.pth_path = osp.join(self.build_directory, 'python', 'pure_python.pth')
+        python_dir = osp.join(self.build_directory, 'python')
+        if not osp.exists(python_dir):
+            os.mkdir(python_dir)
+        self.pth_path = osp.join(python_dir, 'pure_python.pth')
 
     def configure(self):
+        # Make sure file in self.pth_path contains self.source_directory
         if osp.exists(self.pth_path):
             directories = open(self.pth_path).read().split()
         else:
@@ -16,5 +46,22 @@ class PurePythonComponentBuild(object):
             directories.append(self.source_directory)
             open(self.pth_path,'w').write('\n'.join(directories))
         
+        # Create <build directory>/build_files/<component>_src/CMakeLists.txt
+        src_directory = osp.join(self.build_directory, 'build_files', 
+                                 '%s_src' % self.component_name)
+        if not osp.exists(src_directory):
+            os.makedirs(src_directory)
+        cmakelists_content = cmake_template % dict(component_name=self.component_name,
+            source_directory=self.source_directory)
+        cmakelists_path = osp.join(src_directory, 'CMakeLists.txt')
+        write_cmakelists = False
+        if osp.exists(cmakelists_path):
+            write_cmakelists = (open(cmakelists_path).read() != 
+                                cmakelists_content)
+        else:
+            write_cmakelists = True
+        if write_cmakelists:
+            open(cmakelists_path,'w').write(cmakelists_content)
+    
     def build(self):
         pass

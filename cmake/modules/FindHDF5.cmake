09663cb92b913b6bbaa8133f02b39b493944d3a8
denisri <denis.riviere.gm@gmail.com>
2015-10-21 19:10:07 +0000

    set HDF5_LIBRARY variable in cache if found via the system cmake module
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/trunk@69684 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/modules/FindHDF5.cmake
+++ cmake/modules/FindHDF5.cmake
@@ -6,40 +6,49 @@
 # HDF5_LIBRARIES     - the hdf5 libraries
 
 
-# First, try to use the FindHDF5 module included with CMake 2.8 and later
-include("${CMAKE_ROOT}/Modules/FindHDF5.cmake" OPTIONAL)
-
-# Fall back to a simpler detection method
 if( NOT HDF5_FOUND )
 
-  FIND_PATH( HDF5_INCLUDE_DIR hdf5.h
-    ${HDF5_DIR}/include
-    /usr/include
-  )
+  # First, try to use the FindHDF5 module included with CMake 2.8 and later
+  include("${CMAKE_ROOT}/Modules/FindHDF5.cmake" OPTIONAL)
 
-  FIND_LIBRARY( HDF5_LIBRARIES hdf5
-    ${HDF5_DIR}/lib
-    /usr/lib
-  )
-
-  IF( HDF5_INCLUDE_DIR )
-  IF( HDF5_LIBRARIES )
-
-    SET( HDF5_FOUND "YES" )
-
-  ENDIF( HDF5_LIBRARIES )
-  ENDIF( HDF5_INCLUDE_DIR )
-
-  IF( NOT HDF5_FOUND )
-    SET( HDF5_DIR "" CACHE PATH "Root of HDF5 source tree (optional)." )
-    MARK_AS_ADVANCED( HDF5_DIR )
-  ENDIF( NOT HDF5_FOUND )
-endif()
-
-# For compatibility with our older code
-if( HDF5_FOUND )
-  set( HDF5_LIBRARY ${HDF5_LIBRARIES} CACHE FILEPATH "HDF5 library" )
-  if( NOT HDF5_INCLUDE_DIR )
-    set( HDF5_INCLUDE_DIR ${HDF5_C_INCLUDE_DIR} )
+  # for compatibility with our older code
+  if( HDF5_FOUND )
+    set( HDF5_LIBRARY ${HDF5_LIBRARIES} CACHE FILEPATH "HDF5 library" )
   endif()
+
+  # Fall back to a simpler detection method
+  if( NOT HDF5_FOUND )
+
+    FIND_PATH( HDF5_INCLUDE_DIR hdf5.h
+      ${HDF5_DIR}/include
+      /usr/include
+    )
+
+    FIND_LIBRARY( HDF5_LIBRARIES hdf5
+      ${HDF5_DIR}/lib
+      /usr/lib
+    )
+
+    IF( HDF5_INCLUDE_DIR )
+    IF( HDF5_LIBRARIES )
+
+      SET( HDF5_FOUND "YES" )
+
+    ENDIF( HDF5_LIBRARIES )
+    ENDIF( HDF5_INCLUDE_DIR )
+
+    IF( NOT HDF5_FOUND )
+      SET( HDF5_DIR "" CACHE PATH "Root of HDF5 source tree (optional)." )
+      MARK_AS_ADVANCED( HDF5_DIR )
+    ENDIF( NOT HDF5_FOUND )
+  endif()
+
+  # For compatibility with our older code
+  if( HDF5_FOUND )
+    set( HDF5_LIBRARY ${HDF5_LIBRARIES} CACHE FILEPATH "HDF5 library" )
+    if( NOT HDF5_INCLUDE_DIR )
+      set( HDF5_INCLUDE_DIR ${HDF5_C_INCLUDE_DIR} )
+    endif()
+  endif()
+
 endif()

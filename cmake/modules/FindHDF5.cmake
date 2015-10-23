9ccb6b250f9e219bf14412fa8ef142829c3fb2b9
ylep <yann.leprince@cea.fr>
2015-10-23 14:53:49 +0000

    Merge simpler detection of HDF5
    Fixes: #13587
    
    
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/branches/bug_fix@69757 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/modules/FindHDF5.cmake
+++ cmake/modules/FindHDF5.cmake
@@ -1,39 +1,15 @@
 # Try to find the hdf5 library
 # Once done this will define
 #
-# HDF5_FOUND        - system has minc and it can be used
-# HDF5_INCLUDE_DIR  - directory where the header file can be found
-# HDF5_LIBRARIES    - the minc libraries
-#
-# Need to look for Netcdf and hdf5 as well
+# HDF5_FOUND         - system has hdf5 and it can be used
+# HDF5_INCLUDE_DIR - directory where the header file can be found
+# HDF5_LIBRARIES     - the hdf5 libraries
 
-if( NOT HDF5_FOUND )
-  if( CMAKE_VERSION VERSION_GREATER "2.8" )
-      # remove local modules path from CMAKE_MODULE_PATH
-      # we have to take care of duplicated entries pointing to it
-      # with potentially different names (symlinks), so we have to use
-      # realpath for each item.
-      set( _tmp_path ${CMAKE_MODULE_PATH} )
-      get_filename_component( _moddir "${brainvisa-cmake_DIR}/modules"
-        REALPATH )
-      set( _modpaths )
-      foreach( _path ${CMAKE_MODULE_PATH} )
-        get_filename_component( _p ${_path} REALPATH )
-        list( APPEND _modpaths ${_p} )
-      endforeach()
-      list( REMOVE_DUPLICATES _modpaths )
-      list( REMOVE_ITEM _modpaths "${_moddir}" )
-      set(CMAKE_MODULE_PATH ${_modpaths})
-      find_package( HDF5 )
-      set( CMAKE_MODULE_PATH ${_tmp_path} )
 
-      # for compatibility with our older code
-      if( HDF5_FOUND )
-        set( HDF5_LIBRARY ${HDF5_LIBRARIES} )
-      endif()
-  endif()
-endif()
+# First, try to use the FindHDF5 module included with CMake 2.8 and later
+include("${CMAKE_ROOT}/Modules/FindHDF5.cmake" OPTIONAL)
 
+# Fall back to a simpler detection method
 if( NOT HDF5_FOUND )
 
   FIND_PATH( HDF5_INCLUDE_DIR hdf5.h
@@ -46,13 +22,9 @@ if( NOT HDF5_FOUND )
     /usr/lib
   )
 
-
-
   IF( HDF5_INCLUDE_DIR )
   IF( HDF5_LIBRARIES )
 
-    # for compatibility with our older code
-    set( HDF5_LIBRARY ${HDF5_LIBRARIES} CACHE FILEPATH "HDF5 library" )
     SET( HDF5_FOUND "YES" )
 
   ENDIF( HDF5_LIBRARIES )
@@ -62,6 +34,12 @@ if( NOT HDF5_FOUND )
     SET( HDF5_DIR "" CACHE PATH "Root of HDF5 source tree (optional)." )
     MARK_AS_ADVANCED( HDF5_DIR )
   ENDIF( NOT HDF5_FOUND )
-
 endif()
 
+# For compatibility with our older code
+if( HDF5_FOUND )
+  set( HDF5_LIBRARY ${HDF5_LIBRARIES} CACHE FILEPATH "HDF5 library" )
+  if( NOT HDF5_INCLUDE_DIR )
+    set( HDF5_INCLUDE_DIR ${HDF5_C_INCLUDE_DIR} )
+  endif()
+endif()

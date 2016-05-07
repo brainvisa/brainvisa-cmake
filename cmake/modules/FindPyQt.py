a940afc85fcbe07ed687cbbb281db303bdc7c1e4
denisri <denis.riviere.gm@gmail.com>
2016-05-07 23:26:44 +0000

    FindPyQt5 function
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/branches/bug_fix@73032 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/modules/FindPyQt.py
+++ cmake/modules/FindPyQt.py
@@ -30,24 +30,70 @@
 # The fact that you are presently reading this means that you have had
 # knowledge of the CeCILL-B license and that you accept its terms.
 
-import PyQt4.pyqtconfig
+from __future__ import print_function
 
-pyqtcfg = PyQt4.pyqtconfig.Configuration()
-print("pyqt_version:%06.0x" % pyqtcfg.pyqt_version)
-print("pyqt_version_str:%s" % pyqtcfg.pyqt_version_str)
+import sys
 
-pyqt_version_tag = ""
-in_t = False
-for item in pyqtcfg.pyqt_sip_flags.split(' '):
-    if item=="-t":
-        in_t = True
-    elif in_t:
-        if item.startswith("Qt_4"):
-            pyqt_version_tag = item
-    else:
-        in_t = False
-print("pyqt_version_tag:%s" % pyqt_version_tag)
+pyqt_ver = 4
 
-print("pyqt_sip_dir:%s" % pyqtcfg.pyqt_sip_dir)
-print("pyqt_sip_flags:%s" % pyqtcfg.pyqt_sip_flags)
+if len(sys.argv) >= 2:
+  pyqt_ver = int(sys.argv[1])
+
+if pyqt_ver == 5:
+    import os
+    import PyQt5.QtCore
+
+    print("pyqt_version:%06.0x" % PyQt5.QtCore.PYQT_VERSION)
+    print("pyqt_version_str:%s" % PyQt5.QtCore.PYQT_VERSION_STR)
+
+    pyqtcfg = PyQt5.QtCore.PYQT_CONFIGURATION
+    pyqt_version_tag = ""
+    in_t = False
+    for item in pyqtcfg["sip_flags"].split(' '):
+        if item=="-t":
+            in_t = True
+        elif in_t:
+            if item.startswith("Qt_5"):
+                pyqt_version_tag = item
+        else:
+            in_t = False
+    print("pyqt_version_tag:%s" % pyqt_version_tag)
+
+    # finding .sip files location seems not obvioous with PyQt5.
+    sip_dir = "/usr/share/sip/PyQt5"
+    if not os.path.isdir(sip_dir):
+        # Trying a trick with PyQt4
+        try:
+            import PyQt4.pyqtconfig
+
+            pyqtcfg = PyQt4.pyqtconfig.Configuration()
+            sip4_dir = pyqtcfg.pyqt_sip_dir
+            sip_dir = os.path.join(os.path.dirname(sip4_dir, "PyQt5"))
+        except:
+            pass
+
+    print("pyqt_sip_dir:%s" % sip_dir)
+    print("pyqt_sip_flags:%s" % pyqtcfg["sip_flags"])
+
+elif pyqt_ver == 4:
+    import PyQt4.pyqtconfig
+
+    pyqtcfg = PyQt4.pyqtconfig.Configuration()
+    print("pyqt_version:%06.0x" % pyqtcfg.pyqt_version)
+    print("pyqt_version_str:%s" % pyqtcfg.pyqt_version_str)
+
+    pyqt_version_tag = ""
+    in_t = False
+    for item in pyqtcfg.pyqt_sip_flags.split(' '):
+        if item=="-t":
+            in_t = True
+        elif in_t:
+            if item.startswith("Qt_4"):
+                pyqt_version_tag = item
+        else:
+            in_t = False
+    print("pyqt_version_tag:%s" % pyqt_version_tag)
+
+    print("pyqt_sip_dir:%s" % pyqtcfg.pyqt_sip_dir)
+    print("pyqt_sip_flags:%s" % pyqtcfg.pyqt_sip_flags)
 

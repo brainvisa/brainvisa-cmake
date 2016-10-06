50458dbc5976c435990fe6add600a3c66d481ace
denisri <denis.riviere.gm@gmail.com>
2016-10-06 10:01:11 +0000

    force g++-4.9 by default on Ubuntu 16.04 to avoid bugs of g++-5.3
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/branches/bug_fix@74817 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/specific/linux_distribution/ubuntu.cmake
+++ cmake/specific/linux_distribution/ubuntu.cmake
@@ -1,1 +1,13 @@
 message( STATUS "Add specific paths for Ubuntu distribution" )
+
+file( READ /etc/lsb-release _x )
+string( REGEX MATCH "DISTRIB_RELEASE=([0-9.]+)" _x "${_x}" )
+if( CMAKE_MATCH_1 STREQUAL "16.04" )
+    if( EXISTS /usr/bin/g++-4.9 )
+        message( "Ubuntu 16.04, forcing g++ 4.9" )
+        set( CMAKE_CXX_COMPILER /usr/bin/g++-4.9 CACHE FILEPATH 
+            "CXX compiler" )
+    else()
+        message( "Warning, g++-4.9 not found, expect runtime problems with g++-5.3 on Ubuntu 16.04" )
+    endif()
+endif()

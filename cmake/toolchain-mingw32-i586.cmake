099173c576bf7939b72dca165e56b83d2857403b
nsouedet <nicolas.souedet@gmail.com>
2016-10-11 08:57:45 +0000

    Adapt mingw32 toolchain
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/branches/bug_fix@74917 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/toolchain-mingw32-i586.cmake
+++ cmake/toolchain-mingw32-i586.cmake
@@ -1,12 +1,18 @@
 # the name of the target operating system
 SET(CMAKE_SYSTEM_NAME Windows)
 
+if (NOT COMPILER_PREFIX)
+    set(COMPILER_PREFIX "i586-mingw32msvc")
+endif()
+
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER i586-mingw32msvc-gcc)
-SET(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
+find_program(CMAKE_RC_COMPILER NAMES ${COMPILER_PREFIX}-windres)
+find_program(CMAKE_C_COMPILER NAMES ${COMPILER_PREFIX}-gcc)
+find_program(CMAKE_CXX_COMPILER NAMES ${COMPILER_PREFIX}-g++)
+find_program(CMAKE_Fortran_COMPILER NAMES ${COMPILER_PREFIX}-gfortran)
 
 # here is the target environment located
-SET(CMAKE_FIND_ROOT_PATH  /usr/i586-mingw32msvc ${CMAKE_FIND_ROOT_PATH} )
+set(CMAKE_FIND_ROOT_PATH /usr/${COMPILER_PREFIX} ${CMAKE_FIND_ROOT_PATH})
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 

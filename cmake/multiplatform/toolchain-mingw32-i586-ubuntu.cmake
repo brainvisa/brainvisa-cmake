998d67f78837b1b77a7e8632b9dd0ff0dbc7677a
nsouedet <nicolas.souedet@gmail.com>
2016-10-11 08:55:57 +0000

    Adapt toolchain file for mingw32
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/branches/bug_fix@74916 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/multiplatform/toolchain-mingw32-i586-ubuntu.cmake
+++ cmake/multiplatform/toolchain-mingw32-i586-ubuntu.cmake
@@ -1,16 +1,22 @@
 # the name of the target operating system
-set( CMAKE_SYSTEM_NAME Windows )
+SET(CMAKE_SYSTEM_NAME Windows)
+
+if (NOT COMPILER_PREFIX)
+    set(COMPILER_PREFIX "i586-mingw32msvc")
+endif()
 
 # which compilers to use for C and C++
-set( CMAKE_C_COMPILER i586-mingw32msvc-gcc )
-set( CMAKE_CXX_COMPILER i586-mingw32msvc-g++ )
+find_program(CMAKE_RC_COMPILER NAMES ${COMPILER_PREFIX}-windres)
+find_program(CMAKE_C_COMPILER NAMES ${COMPILER_PREFIX}-gcc)
+find_program(CMAKE_CXX_COMPILER NAMES ${COMPILER_PREFIX}-g++)
+find_program(CMAKE_Fortran_COMPILER NAMES ${COMPILER_PREFIX}-gfortran)
 
 # here is the target environment located
-set( CMAKE_FIND_ROOT_PATH /usr/i586-mingw32msvc "${BRAINVISA_CROSSCOMPILATION_DIR}/linux" "${BRAINVISA_CROSSCOMPILATION_DIR}/windows" )
+set(CMAKE_FIND_ROOT_PATH /usr/${COMPILER_PREFIX} ${CMAKE_FIND_ROOT_PATH})
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
 # programs in the host environment
-set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
-set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
-set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)

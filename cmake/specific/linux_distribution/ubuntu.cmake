9c85a6f19436ebf1d9d5c38af150d8286c051ecb
denisri <denis.riviere.gm@gmail.com>
2016-11-07 00:10:50 +0000

    force C++11 in all Ubuntu releases later than 16.04
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/branches/bug_fix@75346 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/specific/linux_distribution/ubuntu.cmake
+++ cmake/specific/linux_distribution/ubuntu.cmake
@@ -8,12 +8,15 @@ set(CMAKE_EXECUTABLE_FORMAT "ELF")
 
 file( READ /etc/lsb-release _x )
 string( REGEX MATCH "DISTRIB_RELEASE=([0-9.]+)" _x "${_x}" )
-if( CMAKE_MATCH_1 STREQUAL "16.04" )
+if( CMAKE_MATCH_1 VERSION_EQUAL "16.04"
+    OR CMAKE_MATCH_1 VERSION_GREATER "16.04" )
     string( FIND "${CMAKE_CXX_FLAGS}" "-std=c++11" _result )
     if( _result EQUAL -1 )
         set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11" CACHE STRING 
             "Flags used by the compiler during all build types." FORCE )
     endif()
+endif()
+if( CMAKE_MATCH_1 STREQUAL "16.04" )
     if( EXISTS /usr/bin/g++-4.9 )
         message( "Ubuntu 16.04, forcing g++ 4.9" )
         set( CMAKE_CXX_COMPILER /usr/bin/g++-4.9 CACHE FILEPATH 

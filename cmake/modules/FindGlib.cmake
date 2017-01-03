ee554eab4c275a3e0b6dfd90cbad251b5961b945
denisri <denis.riviere.gm@gmail.com>
2017-01-03 18:23:33 +0000

    package gthread
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/branches/bug_fix@75915 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/modules/FindGlib.cmake
+++ cmake/modules/FindGlib.cmake
@@ -12,8 +12,14 @@ find_package(PkgConfig)
 if(PKG_CONFIG_FOUND) # Glib search is supported only through pkg_config.
   pkg_search_module(_GLIB glib-2.0)
   if(_GLIB_FOUND)
-    find_library( GLIB_LIBRARIES ${_GLIB_LIBRARIES}
+    find_library( GLIB_LIBRARIES1 ${_GLIB_LIBRARIES}
                   PATHS ${_GLIB_LIBRARY_DIRS} )
+    pkg_search_module(_GLIB gthread-2.0)
+    if(_GTHREAD_FOUND)
+      find_library( GTHREAD_LIBRARIES ${_GTHREAD_LIBRARIES}
+                    PATHS ${_GTHREAD_LIBRARY_DIRS} )
+    endif()
+    set( GLIB_LIBRARIES ${GLIB_LIBRARIES1} ${GTHREAD_LIBRARIES} CACHE PATH "paths to Glib/Gthread lib files" )
     set( GLIB_INCLUDE_DIRS ${_GLIB_INCLUDE_DIRS} CACHE PATH "paths to Glib header files" )
     set( GLIB_VERSION "${_GLIB_VERSION}" CACHE STRING "version of Glib library")
 

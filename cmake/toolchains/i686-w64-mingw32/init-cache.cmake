29e3f8fda0b0531d32cac37a1c8a19a06452c110
nsouedet <nicolas.souedet@gmail.com>
2017-01-18 16:24:55 +0000

    fixes: issue in distcc cross compilation
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/branches/bug_fix@76081 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/toolchains/i686-w64-mingw32/init-cache.cmake
+++ cmake/toolchains/i686-w64-mingw32/init-cache.cmake
@@ -7,9 +7,11 @@ if (NOT COMPILER_PREFIX)
     set(COMPILER_PREFIX "i686-w64-mingw32")
 endif()
 
+# Add brainvisa root directory
 set( CMAKE_FIND_ROOT_PATH
      ${BRAINVISA_CROSSCOMPILATION_DIR} 
      ${CMAKE_FIND_ROOT_PATH} )
+
 set( PYTHON_EXECUTABLE "/usr/bin/python2.7" 
      CACHE FILEPATH "Python executable" )
 set( CROSSCOMPILING_PYTHON_EXECUTABLE
@@ -33,4 +35,4 @@ set( SIP_VERSION "${SIP_VERSION}"
      CACHE STRING "Version of sip executable" )
 set( NUMPY_INCLUDE_DIR "${BRAINVISA_CROSSCOMPILATION_DIR}/python/Lib/site-packages/numpy/core/include"
      CACHE PATH "Numpy include" )
-    
+     

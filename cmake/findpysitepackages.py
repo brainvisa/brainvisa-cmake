f93138949916c8050ac7ebd27818d5176dc5ed4f
denisri <denis.riviere.gm@gmail.com>
2017-04-13 16:03:06 +0000

    fix in findpysitepackages.py and check uncopiable file flags when copying trees
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/branches/bug_fix@77223 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/findpysitepackages.py
+++ cmake/findpysitepackages.py
@@ -8,7 +8,7 @@ exclude = set()
 if len(sys.argv) >= 2:
     exclude.update(sys.argv[1].split(';'))
 for p in sys.path:
-    if os.path.basename(p) == 'site-packages' and p not in dirs and p not in exclude and not any([p.startswith(p2 + os.sep) for p2 in dirs]):
+    if os.path.basename(p) == 'site-packages' and p not in dirs and p not in exclude and not any([p.startswith(p2 + os.sep) for p2 in exclude]):
         to_del = []
         for p2 in dirs:
             if p2.startswith(p + os.sep):
@@ -16,5 +16,6 @@ for p in sys.path:
         for p2 in to_del:
             dirs.remove(p2)
         dirs.append(p)
+        exclude.add(p)
 
 sys.stdout.write(';'.join(dirs))

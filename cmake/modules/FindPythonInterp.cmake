fda5c20e4b90fbbe0a47a9082e5f6f4eec79c371
nsouedet <nicolas.souedet@gmail.com>
2017-05-31 10:36:12 +0000

    fixes: keep standard compatibility for FindPythonInterp.cmake
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/branches/bug_fix@77791 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/modules/FindPythonInterp.cmake
+++ cmake/modules/FindPythonInterp.cmake
@@ -20,37 +20,44 @@
 # implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the License for more information.
 #=============================================================================
-if(NOT PYTHON_HOST_EXECUTABLE OR NOT PYTHON_EXECUTABLE)
-    # (To distributed this file outside of CMake, substitute the full
-    #  License text for the above reference.)
-    FIND_PROGRAM(PYTHON_HOST_EXECUTABLE
-        NAMES python2.7 python2.6 python2 python
-            python3 python3.6 python3.5 python3.4 python3.3 python3.2 python3.1 python3.0
-        PATHS
-            [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\3.6\\InstallPath]
-            [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\3.5\\InstallPath]
-            [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\3.4\\InstallPath]
-            [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\3.3\\InstallPath]
-            [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\3.2\\InstallPath]
-            [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\3.1\\InstallPath]
-            [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\3.0\\InstallPath]
-            [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.7\\InstallPath]
-            [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.6\\InstallPath]
-    )
-    
-    if(NOT PYTHON_EXECUTABLE)
-        # Also get target python interpreter information if possible
-        if(CMAKE_CROSSCOMPILING)
-            if(CROSSCOMPILING_PYTHON_EXECUTABLE)
-                # Get target python interpreter information
-                set(PYTHON_EXECUTABLE "${CROSSCOMPILING_PYTHON_EXECUTABLE}" 
-                    CACHE FILEPATH "Target python executable path")
+if(NOT CMAKE_CROSSCOMPILING AND NOT PYTHON_HOST_EXECUTABLE AND PYTHON_EXECUTABLE)
+    # Sets PYTHON_HOST_EXECUTABLE from PYTHON_EXECUTABLE to keep standard 
+    # compatibility
+    set(PYTHON_HOST_EXECUTABLE "${PYTHON_EXECUTABLE}" 
+        CACHE FILEPATH "Python executable path")
+else()
+    if(NOT PYTHON_HOST_EXECUTABLE OR NOT PYTHON_EXECUTABLE)
+        # (To distributed this file outside of CMake, substitute the full
+        #  License text for the above reference.)
+        FIND_PROGRAM(PYTHON_HOST_EXECUTABLE
+            NAMES python2.7 python2.6 python2 python
+                python3 python3.6 python3.5 python3.4 python3.3 python3.2 python3.1 python3.0
+            PATHS
+                [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\3.6\\InstallPath]
+                [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\3.5\\InstallPath]
+                [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\3.4\\InstallPath]
+                [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\3.3\\InstallPath]
+                [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\3.2\\InstallPath]
+                [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\3.1\\InstallPath]
+                [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\3.0\\InstallPath]
+                [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.7\\InstallPath]
+                [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.6\\InstallPath]
+        )
+        
+        if(NOT PYTHON_EXECUTABLE)
+            # Also get target python interpreter information if possible
+            if(CMAKE_CROSSCOMPILING)
+                if(CROSSCOMPILING_PYTHON_EXECUTABLE)
+                    # Get target python interpreter information
+                    set(PYTHON_EXECUTABLE "${CROSSCOMPILING_PYTHON_EXECUTABLE}" 
+                        CACHE FILEPATH "Target python executable path")
+                endif()
+            else()
+                # When not cross compiling, setting PYTHON_EXECUTABLE 
+                # to PYTHON_HOST_EXECUTABLE
+                set(PYTHON_EXECUTABLE "${PYTHON_HOST_EXECUTABLE}" 
+                    CACHE FILEPATH "Target python executable path")    
             endif()
-        else()
-            # When not cross compiling, setting PYTHON_EXECUTABLE 
-            # to PYTHON_HOST_EXECUTABLE
-            set(PYTHON_EXECUTABLE "${PYTHON_HOST_EXECUTABLE}" 
-                CACHE FILEPATH "Target python executable path")    
         endif()
     endif()
 endif()

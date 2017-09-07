3ec5e464f6a7286f566f97cf96c5918f97332884
denisri <denis.riviere.gm@gmail.com>
2017-09-07 14:55:10 +0000

    try to find netcdf using pkg-config
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/branches/bug_fix@79077 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/modules/FindNETCDF.cmake
+++ cmake/modules/FindNETCDF.cmake
@@ -1,51 +1,100 @@
 # Try to find the minc library
 # Once done this will define
 #
-# NETCDF_FOUND        - system has minc and it can be used
+# NETCDF_FOUND        - system has netcdf and it can be used
 # NETCDF_INCLUDE_DIR  - directory where the header file can be found
-# NETCDF_LIBRARIES    - the minc libraries
+# NETCDF_LIBRARIES    - the netcdf libraries
+# NETCDF_LIBRARY      - the netcdf libraries
 # NETCDF_NEEDS_MPI    - TRUE if NetCDF is built using MPI library
 #
 # Need to look for Netcdf and hdf5 as well
 
-if(NETCDF_INCLUDE_DIR AND NETCDF_LIBRARY)
+if( NETCDF_INCLUDE_DIR AND NETCDF_LIBRARY )
     SET( NETCDF_FOUND "YES" )
 else()
-    FIND_PATH( NETCDF_INCLUDE_DIR netcdf.h
-        ${NETCDF_DIR}/include
-        ${NETCDF_DIR}/include/netcdf-3
-        ${NETCDF_DIR}/include/netcdf
-        /usr/include 
-        /usr/include/netcdf-3 
-        /usr/include/netcdf
-    )
 
-    FIND_LIBRARY( NETCDF_LIBRARY netcdf
-        ${NETCDF_DIR}/lib
-        /usr/lib
-    )
+    find_package( PkgConfig QUIET )
+    if( PkgConfig_FOUND )
 
+        pkg_check_modules( PC_NETCDF QUIET netcdf )
 
+    endif()
+    if( PC_NETCDF_FOUND )
 
-    IF( NETCDF_INCLUDE_DIR )
-        # check dependance on mpi
-        file(READ "${NETCDF_INCLUDE_DIR}/netcdf.h" _netcdf_h )
-        string(REGEX MATCH "#include <mpi.h>" _mpi_inc ${_netcdf_h} )
-        if( _mpi_inc )
-            find_package( MPI )
-            set( NETCDF_INCLUDE_DIR ${NETCDF_INCLUDE_DIR} ${MPI_C_INCLUDE_PATH}
-                 CACHE PATH "NetCDF include path" FORCE )
-            set( NETCDF_NEEDS_MPI "TRUE"
-                 CACHE STRING "if NetCDF is built using MPI library" )
-        endif()
+        # configured via pkg-config
+        set( NETCDF_INCLUDE_DIR
+             ${PC_NETCDF_INCLUDEDIR} ${PC_NETCDF_INCLUDE_DIRS}
+             CACHE PATH "NetCDF include path" )
+        set( NETCDF_LIBRARIES )
+        foreach( lib ${PC_NETCDF_LIBRARIES} )
+          find_library( netcdf_lib ${lib}
+                        PATHS ${PC_NETCDF_LIBRARY_DIRS} )
+          list( APPEND NETCDF_LIBRARIES ${netcdf_lib} )
+          unset( netcdf_lib CACHE )
+        endforeach()
+        set( NETCDF_LIBRARIES ${NETCDF_LIBRARIES} CACHE PATH "NetCDF libraries")
+        set( NETCDF_LIBRARY ${NETCDF_LIBRARIES} CACHE PATH "NetCDF libraries")
+        set( NETCDF_FOUND "YES" )
+
+    endif()
+
+    if( NOT NETCDF_INCLUDE_DIR ) #OR NOT EXISTS "${NETCDF_INCLUDE_DIR}/netcdf.h" )
+        # pkg-config does not work: try the manual way
+
+        unset( NETCDF_INCLUDE_DIR )
+        unset( NETCDF_INCLUDE_DIR CACHE )
+
+        FIND_PATH( NETCDF_INCLUDE_DIR netcdf.h
+            ${NETCDF_DIR}/include
+            ${NETCDF_DIR}/include/netcdf-3
+            ${NETCDF_DIR}/include/netcdf
+            /usr/include
+            /usr/include/netcdf-3
+            /usr/include/netcdf
+        )
+
+        FIND_LIBRARY( NETCDF_LIBRARY netcdf
+            ${NETCDF_DIR}/lib
+            /usr/lib
+        )
 
         IF( NETCDF_LIBRARY )
             SET( NETCDF_FOUND "YES" )
+            set( NETCDF_LIBRARIES ${NETCDF_LIBRARY}
+                 CACHE PATH "NetCDF libraries")
         ENDIF( NETCDF_LIBRARY )
-    ENDIF( NETCDF_INCLUDE_DIR )
 
-    IF( NOT NETCDF_FOUND )
+    endif()
+
+
+    if( NETCDF_FOUND )
+
+        IF( NETCDF_INCLUDE_DIR )
+            # check dependance on mpi
+            list( GET NETCDF_INCLUDE_DIR 0 incdir )
+            file(READ "${incdir}/netcdf.h" _netcdf_h )
+            string(REGEX MATCH "#include <mpi.h>" _mpi_inc ${_netcdf_h} )
+            if( _mpi_inc )
+                find_package( MPI )
+                set( NETCDF_INCLUDE_DIR ${NETCDF_INCLUDE_DIR} ${MPI_C_INCLUDE_PATH}
+                    CACHE PATH "NetCDF include path" FORCE )
+                set( NETCDF_NEEDS_MPI "TRUE"
+                    CACHE STRING "if NetCDF is built using MPI library" )
+            endif()
+
+        ENDIF( NETCDF_INCLUDE_DIR )
+
+    else()
+
         SET( NETCDF_DIR "" CACHE PATH "Root of NETCDF source tree (optional)." )
         MARK_AS_ADVANCED( NETCDF_DIR )
-    ENDIF( NOT NETCDF_FOUND )
+
+    endif()
+
+#     message("*** NETCDF config:")
+#     message("NETCDF_FOUND: ${NETCDF_FOUND}")
+#     message("NETCDF_INCLUDE_DIR: ${NETCDF_INCLUDE_DIR}")
+#     message("NETCDF_LIBRARIES: ${NETCDF_LIBRARIES}")
+#     message("NETCDF_LIBRARY: ${NETCDF_LIBRARY}")
+#     message("NETCDF_NEEDS_MPI: ${NETCDF_NEEDS_MPI}")
 endif()

4a18fdcbc12734956a111a924d474e490ff26fda
nsouedet <nicolas.souedet@gmail.com>
2018-06-18 14:16:40 +0000

    fixes: centos issues
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/branches/bug_fix@82000 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ cmake/specific/linux_distribution/centos.cmake
@@ -0,0 +1,18 @@
+message( STATUS "Add specific paths for Ubuntu distribution" )
+
+# something has changed somewhere, the ELF format is not recognized by default
+# any longer and makes install fail because of a rpath/relink step missing.
+# The solution is to force ELF format
+# see https://cmake.org/Bug/view.php?id=13934#c37157
+set(CMAKE_EXECUTABLE_FORMAT "ELF")
+
+file( READ /etc/redhat-release _x )
+string( REGEX MATCH "release ([0-9.]+)" _x "${_x}" )
+if( CMAKE_MATCH_1 VERSION_EQUAL "7"
+    OR CMAKE_MATCH_1 VERSION_GREATER "7" )
+    string( FIND "${CMAKE_CXX_FLAGS}" "-std=c++11" _result )
+    if( _result EQUAL -1 )
+        set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11" CACHE STRING 
+            "Flags used by the compiler during all build types." FORCE )
+    endif()
+endif()

de4bdd4c80dc648c61404ec8883adae05153ba96
denisri <denis.riviere.gm@gmail.com>
2018-08-27 17:25:48 +0000

    fixes
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/branches/bug_fix@82852 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- cmake/brainvisa-packaging-libqt5qml.cmake
+++ cmake/brainvisa-packaging-libqt5qml.cmake
@@ -6,8 +6,10 @@ function( BRAINVISA_PACKAGING_COMPONENT_INFO component package_name package_main
   set( ${package_version} "${QT_VERSION}" PARENT_SCOPE )
 
   BRAINVISA_THIRDPARTY_DEPENDENCY( "${component}" RUN DEPENDS libqt5network RUN )
-  BRAINVISA_THIRDPARTY_DEPENDENCY( "${component}" RUN
-                                    DEPENDS libdoubleconversion RUN )
+  if( NOT APPLE AND NOT WIN32 )
+    BRAINVISA_THIRDPARTY_DEPENDENCY( "${component}" RUN
+                                     DEPENDS libdoubleconversion RUN )
+  endif()
 endfunction()
 
 function( BRAINVISA_PACKAGING_COMPONENT_RUN component )

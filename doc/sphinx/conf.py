41e4c21af5f5f23f5252a9955b83d0f3147e9b17
Denis Rivière <denis.riviere.gm@gmail.com>
2018-11-28 17:17:13 +0100

    fixed date in doc

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -13,6 +13,7 @@
 
 from __future__ import print_function
 import sys, os
+import datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -68,7 +69,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'BrainVisa-Cmake'
-copyright = u'2016, CEA'
+copyright = u'%s, CEA' % datetime.datetime.now().year
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

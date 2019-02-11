12db02851f79d43e8cad964a74c0813f6b9c847c
Yann Leprince <yann.leprince@cea.fr>
2019-02-11 11:01:24 +0100

    Ease debugging of force-set CMake cache variables

--- cmake/specific/linux_distribution/ubuntu.cmake
+++ cmake/specific/linux_distribution/ubuntu.cmake
@@ -12,8 +12,8 @@ if( CMAKE_MATCH_1 VERSION_EQUAL "16.04"
     OR CMAKE_MATCH_1 VERSION_GREATER "16.04" )
     string( FIND "${CMAKE_CXX_FLAGS}" "-std=c++11" _result )
     if( _result EQUAL -1 )
-        set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11" CACHE STRING 
-            "Flags used by the compiler during all build types." FORCE )
+        set( CMAKE_CXX_FLAGS "-std=c++11 ${CMAKE_CXX_FLAGS}" CACHE STRING
+            "Flags used by the compiler during all build types (flags forced by brainvisa-cmake)." FORCE )
     endif()
 endif()
 if( CMAKE_MATCH_1 STREQUAL "16.04" )
@@ -21,13 +21,13 @@ if( CMAKE_MATCH_1 STREQUAL "16.04" )
         if( EXISTS /usr/bin/g++-4.9 )
             message("Ubuntu 16.04, trying not too hard to use g++-4.9")
             set( CMAKE_CXX_COMPILER /usr/bin/g++-4.9 CACHE FILEPATH 
-                 "CXX compiler" )
+                 "CXX compiler (gcc-4.9 set by brainvisa-cmake)" )
         endif()
     else()
         if( EXISTS /usr/bin/g++-4.9 )
             message( "Ubuntu 16.04, forcing use of g++ 4.9. If you need to use another compiler, please set CMake variable BRAINVISA_IGNORE_BUG_GCC_5=YES" )
             set( CMAKE_CXX_COMPILER /usr/bin/g++-4.9 CACHE FILEPATH 
-                 "CXX compiler" FORCE )
+                 "CXX compiler (g++-4.9 forced by brainvisa-cmake)" FORCE )
         else()
             message(FATAL_ERROR "g++-4.9 not found, expect runtime problems with g++-5.3 on Ubuntu 16.04. Please install g++-4.9 package or set CMake variable BRAINVISA_IGNORE_BUG_GCC_5=YES" )
         endif()

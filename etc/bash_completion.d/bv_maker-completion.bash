380e5647290da4ae8427a88adce76054ad9de052
Denis Rivière <denis.riviere.gm@gmail.com>
2019-04-12 16:37:31 +0200

    bash completion for bv_maker: added context for options depending on a subcommand

--- etc/bash_completion.d/bv_maker-completion.bash
+++ etc/bash_completion.d/bv_maker-completion.bash
@@ -7,6 +7,64 @@ function _complete_bv_maker_()
     local cmd_list="info sources configure build doc testref test pack install_pack testref_pack test_pack publish_pack"
     local opt_list="-c -h -d -s -b --username -e --email --disable-jenkins --def --only-if-default -v --verbose --version"
 
+    # get last subcommand
+    local i=0
+    local last_cmd=
+    while [ $i -ne $COMP_CWORD ]; do
+        local _cw=${COMP_WORDS[$i]}
+        i=$(( i + 1 ))
+        local j=0
+        for j in $cmd_list; do
+            if [ "$_cw" = "$j" ]; then
+                last_cmd="$j"
+                break
+            fi
+        done
+    done
+
+#     if [ -n "$last_cmd" ]; then
+#         echo "last cmd: $last_cmd"
+#     fi
+
+    # options are specific to each subcommand
+    case "$last_cmd" in
+    info)
+        opt_list="-h --help --only-if-default"
+        ;;
+    sources)
+        opt_list="-h --help --only-if-default --no-cleanup --no-svn --no-git --ignore-git-failure"
+        ;;
+    configure)
+        opt_list="-h --help -c --clean --only-if-default"
+        ;;
+    build)
+        opt_list="-h --help -c --clean --only-if-default"
+        ;;
+    doc)
+        opt_list="-h --help --only-if-default"
+        ;;
+    testref)
+        opt_list="-h --help --only-if-default -m --make_options"
+        ;;
+    test)
+        opt_list="-h --help --only-if-default -t --ctest_options"
+        ;;
+    pack)
+        opt_list="-h --help --only-if-default"
+        ;;
+    install_pack)
+        opt_list="-h --help --only-if-default --package-date --package-time --package-version --prefix --local --offline"
+        ;;
+    testref_pack)
+        opt_list="-h --help --only-if-default -m --make_options --package-date --package-time --package-version"
+        ;;
+    test_pack)
+        opt_list="-h --help --only-if-default -t --ctest_options --package-date --package-time --package-version"
+        ;;
+    *)
+        ;;
+    esac
+
     COMPREPLY=($(compgen -W "$cmd_list $opt_list" -- "${word}"))
     if [ -n "$COMPREPLY" ]; then
         COMPREPLY="$COMPREPLY "

fd6b5978136061e7fe18ff6f0daedbb9a8655d06
Nicolas Souedet <nicolas.souedet@cea.fr>
2019-07-15 12:18:59 +0200

    fixes: On Ubuntu 16.04 the default forced use of /usr/bin/g++-4.9 does not allow to to use g++ through distcc, so fixed it searching for g++-4.9 using the PATH

--- cmake/specific/linux_distribution/ubuntu.cmake
+++ cmake/specific/linux_distribution/ubuntu.cmake
@@ -17,16 +17,17 @@ if( CMAKE_MATCH_1 VERSION_EQUAL "16.04"
     endif()
 endif()
 if( CMAKE_MATCH_1 STREQUAL "16.04" )
+    find_program(GCC_4_9 g++-4.9)
     if( BRAINVISA_IGNORE_BUG_GCC_5 )
-        if( EXISTS /usr/bin/g++-4.9 )
-            message("Ubuntu 16.04, trying not too hard to use g++-4.9")
-            set( CMAKE_CXX_COMPILER /usr/bin/g++-4.9 CACHE FILEPATH 
+        if( EXISTS ${GCC_4_9} )
+            message("Ubuntu 16.04, trying not too hard to use g++-4.9 (${GCC_4_9})")
+            set( CMAKE_CXX_COMPILER ${GCC_4_9} CACHE FILEPATH 
                  "CXX compiler (gcc-4.9 set by brainvisa-cmake)" )
         endif()
     else()
-        if( EXISTS /usr/bin/g++-4.9 )
-            message( "Ubuntu 16.04, forcing use of g++ 4.9. If you need to use another compiler, please set CMake variable BRAINVISA_IGNORE_BUG_GCC_5=YES" )
-            set( CMAKE_CXX_COMPILER /usr/bin/g++-4.9 CACHE FILEPATH 
+        if( EXISTS ${GCC_4_9} )
+            message( "Ubuntu 16.04, forcing use of g++ 4.9 (${GCC_4_9}). If you need to use another compiler, please set CMake variable BRAINVISA_IGNORE_BUG_GCC_5=YES" )
+            set( CMAKE_CXX_COMPILER ${GCC_4_9} CACHE FILEPATH 
                  "CXX compiler (g++-4.9 forced by brainvisa-cmake)" FORCE )
         else()
             message(FATAL_ERROR "g++-4.9 not found, expect runtime problems with g++-5.3 on Ubuntu 16.04. Please install g++-4.9 package or set CMake variable BRAINVISA_IGNORE_BUG_GCC_5=YES" )

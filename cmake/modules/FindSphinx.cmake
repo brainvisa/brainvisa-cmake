e4214a1551931ecb862fe667505e1ac338308bc0
Denis Rivière <denis.riviere.gm@gmail.com>
2020-09-08 11:33:02 +0200

    find and use sphinx as "$PYTHON_EXECUTABLE -m sphinx" in cmake

--- cmake/modules/FindSphinx.cmake
+++ cmake/modules/FindSphinx.cmake
@@ -10,33 +10,40 @@ if( SPHINX_VERSION )
   # Sphinx is already found, do nothing
   set(SPHINX_FOUND TRUE)
 else()
-  find_package( python )
-  if( PYTHON_HOST_SHORT_VERSION VERSION_LESS "3.0" )
-    set(sphinxbuild_cmd sphinx-build)
-  else()
-    set(sphinxbuild_cmd sphinx-build3)
-  endif()
-  find_program( SPHINXBUILD_HOST_EXECUTABLE
-    NAMES ${sphinxbuild_cmd}
-    DOC "Path to sphinx-build executable" )
-
-  # Also set sphinx target variable
-  if(CMAKE_CROSSCOMPILING)
-    find_program(SPHINXBUILD_EXECUTABLE
-        NAMES ${sphinxbuild_cmd}${CMAKE_EXECUTABLE_SUFFIX}
-        ONLY_CMAKE_FIND_ROOT_PATH
-        DOC "Target sphinx executable path" )
-    
-    # Uses target python interpreter
-    #set(SPHINXBUILD_EXECUTABLE "${CROSSCOMPILING_SPHINXBUILD_EXECUTABLE}" CACHE FILEPATH "Target sphinx executable path")
-    get_filename_component(SPHINXBUILD_EXECUTABLE_NAME "${CROSSCOMPILING_SPHINXBUILD_EXECUTABLE}" NAME CACHE)
-  else()
-    # Uses host python interpreter
-    set(SPHINXBUILD_EXECUTABLE "${SPHINXBUILD_HOST_EXECUTABLE}" CACHE FILEPATH "Target sphinx executable path")
-    get_filename_component(SPHINXBUILD_EXECUTABLE_NAME "${SPHINXBUILD_EXECUTABLE}" NAME CACHE)
-  endif()
-
   find_package( python REQUIRED )
+  execute_process( COMMAND ${CMAKE_TARGET_SYSTEM_PREFIX} ${PYTHON_EXECUTABLE}
+                           -m sphinx -h
+                   OUTPUT_QUIET
+                   RESULT_VARIABLE _SPHINX_RES )
+  if( _SPHINX_RES EQUAL 0 )
+    set( SPHINXBUILD_EXECUTABLE ${PYTHON_EXECUTABLE} -m sphinx
+         CACHE STRING "Target sphinx executable path" )
+  endif()
+
+#   if( PYTHON_HOST_SHORT_VERSION VERSION_LESS "3.0" )
+#     set(sphinxbuild_cmd sphinx-build)
+#   else()
+#     set(sphinxbuild_cmd sphinx-build3)
+#   endif()
+#   find_program( SPHINXBUILD_HOST_EXECUTABLE
+#     NAMES ${sphinxbuild_cmd}
+#     DOC "Path to sphinx-build executable" )
+#
+#   # Also set sphinx target variable
+#   if(CMAKE_CROSSCOMPILING)
+#     find_program(SPHINXBUILD_EXECUTABLE
+#         NAMES ${sphinxbuild_cmd}${CMAKE_EXECUTABLE_SUFFIX}
+#         ONLY_CMAKE_FIND_ROOT_PATH
+#         DOC "Target sphinx executable path" )
+#
+#     # Uses target python interpreter
+#     #set(SPHINXBUILD_EXECUTABLE "${CROSSCOMPILING_SPHINXBUILD_EXECUTABLE}" CACHE FILEPATH "Target sphinx executable path")
+#     get_filename_component(SPHINXBUILD_EXECUTABLE_NAME "${CROSSCOMPILING_SPHINXBUILD_EXECUTABLE}" NAME CACHE)
+#   else()
+#     # Uses host python interpreter
+#     set(SPHINXBUILD_EXECUTABLE "${SPHINXBUILD_HOST_EXECUTABLE}" CACHE FILEPATH "Target sphinx executable path")
+#     get_filename_component(SPHINXBUILD_EXECUTABLE_NAME "${SPHINXBUILD_EXECUTABLE}" NAME CACHE)
+#   endif()
 
   mark_as_advanced( SPHINXBUILD_EXECUTABLE )
   if(SPHINXBUILD_EXECUTABLE)
@@ -50,10 +57,10 @@ ver = [int(x) for x in sphinx.__version__.split('.')];
 print('%x' % (ver[0] * 0x10000 + ver[1] * 0x100 + ver[2]))"
                     OUTPUT_VARIABLE SPHINX_VERSION
                     OUTPUT_STRIP_TRAILING_WHITESPACE)
-    set( SPHINX_VERSION "${SPHINX_VERSION}" CACHE STRING "Version of sphinx module" )
+    set( SPHINX_VERSION "${SPHINX_VERSION}" CACHE STRING "Version of sphinx module" FORCE )
     mark_as_advanced( SPHINX_VERSION )
   endif()
-  
+
   if(SPHINXBUILD_EXECUTABLE AND SPHINX_VERSION)
     set( SPHINX_FOUND TRUE )
   else()

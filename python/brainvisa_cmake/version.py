8c1ee015af06caf25ff19453362dfaf23d3b87dc
sapetnioc <sapetnioc@users.noreply.github.com>
2023-05-22 15:21:45 +0200

    #97 make version.py working also from installation directory

--- python/brainvisa_cmake/version.py
+++ python/brainvisa_cmake/version.py
@@ -3,7 +3,11 @@ from pathlib import Path
 
 from brainvisa_cmake.brainvisa_projects import read_project_info
 
-base_dir = Path(__file__).parent.parent.parent
+base_dir = Path(__file__).parent
+if not (base_dir / 'project_info.cmake').exists():
+    base_dir = base_dir.parent.parent
+if not (base_dir / 'project_info.cmake').exists():
+    raise RuntimeError(f'Cannot find brainvisa-cmake project_info.cmake in {base_dir}')
 
 version_major, version_minor, version_patch = read_project_info(str(base_dir))[2]._version_numbers
 

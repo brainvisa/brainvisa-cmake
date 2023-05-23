7b36ea37102d7d02a076ca7ef9ec8e94c66f4072
Denis Rivière <denis.riviere.gm@gmail.com>
2023-05-23 14:15:15 +0200

    attempt to use sip5 and sip ABI version. Failed.

--- cmake/modules/FindSIP.cmake
+++ cmake/modules/FindSIP.cmake
@@ -19,12 +19,22 @@ else( SIP_VERSION )
     if( ( DESIRED_QT_VERSION EQUAL 6 ) AND NOT DESIRED_SIP_VERSION )
       set( DESIRED_SIP_VERSION 6 )
     endif()
-    if( DESIRED_SIP_VERSION EQUAL 6 )
+    if( DESIRED_SIP_VERSION GREATER 5 )
         # Try to find sip in target root path
         find_program( SIP_EXECUTABLE
           NAMES sip-build${CMAKE_EXECUTABLE_SUFFIX}
           ONLY_CMAKE_FIND_ROOT_PATH
           DOC "Path to sip-build executable" )
+        find_program( SIP_MODULE_EXECUTABLE
+          NAMES sip-module${CMAKE_EXECUTABLE_SUFFIX}
+          ONLY_CMAKE_FIND_ROOT_PATH
+          DOC "Path to sip-module executable" )
+    elseif( DESIRED_SIP_VERSION EQUAL 5 )
+        # Try to find sip in target root path
+        find_program( SIP_EXECUTABLE
+          NAMES sip5${CMAKE_EXECUTABLE_SUFFIX}
+          ONLY_CMAKE_FIND_ROOT_PATH
+          DOC "Path to sip executable" )
         find_program( SIP_MODULE_EXECUTABLE
           NAMES sip-module${CMAKE_EXECUTABLE_SUFFIX}
           ONLY_CMAKE_FIND_ROOT_PATH
@@ -47,7 +57,7 @@ else( SIP_VERSION )
       execute_process( COMMAND ${CMAKE_TARGET_SYSTEM_PREFIX} ${SIP_EXECUTABLE} -V OUTPUT_VARIABLE SIP_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE )
       set( SIP_VERSION "${SIP_VERSION}" CACHE STRING "Version of sip executable" )
       mark_as_advanced( SIP_VERSION )
-      if( SIP_VERSION VERSION_LESS 6.0.0 )
+      if( SIP_VERSION VERSION_LESS 5.0.0 )
         execute_process( COMMAND ${CMAKE_TARGET_SYSTEM_PREFIX} ${PYTHON_EXECUTABLE} -c "import sipconfig, sys; sys.stdout.write(sipconfig.Configuration().sip_inc_dir + '\\n')"
           OUTPUT_VARIABLE SIP_INCLUDE_DIR OUTPUT_STRIP_TRAILING_WHITESPACE )
         set( SIP_INCLUDE_DIR "${SIP_INCLUDE_DIR}" CACHE PATH "Path to sip include files" )
@@ -80,16 +90,24 @@ else( SIP_VERSION )
         execute_process( COMMAND ${CMAKE_TARGET_SYSTEM_PREFIX} ${PYTHON_EXECUTABLE} -c "import PyQt${DESIRED_QT_VERSION}.sip"
             RESULT_VARIABLE _new_module )
         if( _new_module EQUAL 0 )
-          set(SIP_FLAGS "-n" "PyQt${DESIRED_QT_VERSION}.sip" CACHE STRING
+          set( SIP_FLAGS "-n" "PyQt${DESIRED_QT_VERSION}.sip" CACHE STRING
               "options passed to SIP program" )
           set( SIP_MODULE "PyQt${DESIRED_QT_VERSION}.sip" CACHE STRING
                "sip python module" )
+          # get the sip ABI version in PyQt
+          find_file( _find_sipabi_py FindSipABI.py PATHS ${CMAKE_MODULE_PATH} )
+          execute_process(COMMAND ${CMAKE_TARGET_SYSTEM_PREFIX} ${PYTHON_HOST_EXECUTABLE} "${_find_sipabi_py}" "${SIP_MODULE}" OUTPUT_VARIABLE SIP_ABI )
         else()
           set( SIP_FLAGS "" CACHE STRING "options passed to SIP program" )
           set( SIP_MODULE "sip" CACHE STRING "sip python module" )
         endif()
-        if( SIP_MODULE_EXECUTABLE )  # sip 6
-          execute_process( COMMAND "${SIP_MODULE_EXECUTABLE}" "--sip-h" "--target-dir" "${CMAKE_BINARY_DIR}/include" "${SIP_MODULE}" )
+        if( SIP_MODULE_EXECUTABLE )  # sip 5/6
+          if( SIP_ABI )
+            set( SIP_ABI ${SIP_ABI} CACHE STRING "SIP ABI version" )
+            ## not taken into account by sip-build, so it is useless and breaks
+            # set( _added_flags "--abi-version" "${SIP_ABI}" )
+          execute_process( COMMAND "${SIP_MODULE_EXECUTABLE}" "--sip-h" "--target-dir" "${CMAKE_BINARY_DIR}/include" ${_added_flags} "${SIP_MODULE}" )
+          endif()
         endif()
       else()
         set( SIP_FLAGS "" CACHE STRING "options passed to SIP program" )

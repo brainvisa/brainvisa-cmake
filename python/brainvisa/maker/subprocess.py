67d654b7783d8842e0157987906c3c482fc0bd2f
Denis Rivière <denis.riviere.gm@gmail.com>
2024-06-26 11:32:01 +0200

    fixed subprocess and timeout for python3

--- python/brainvisa/maker/subprocess.py
+++ python/brainvisa/maker/subprocess.py
@@ -1,28 +1,18 @@
 # -*- coding: utf-8 -*-
 """Tools for launching subprocesses from bv_maker."""
 
-from __future__ import absolute_import, division
-from __future__ import print_function, unicode_literals
-
 import locale
 import os
 import subprocess
 import sys
-
-import six
-from six.moves import shlex_quote
+import signal
+import shlex
 
 
 try:
     from subprocess import DEVNULL
 except ImportError:
     DEVNULL = open(os.devnull, 'wb')
-
-try:
-    # backport of subprocess of python 3
-    import subprocess32
-except ImportError:
-    subprocess32 = None
 
 
 def decode_output(output_bytes):
@@ -31,21 +21,22 @@ def decode_output(output_bytes):
 
 
 def system(*args, **kwargs):
-    print('$ ' + ' '.join(shlex_quote(arg) for arg in args))
+    print('$ ' + ' '.join(shlex.quote(arg) for arg in args))
     error = None
     try:
         try:
             kwargs = dict(kwargs)
             timeout = kwargs.pop('timeout', None)
-            if subprocess32 is not None:
-                popen = subprocess32.Popen(args, **kwargs)
-                popen.communicate(timeout=timeout)
-            else:
-                # timeout not supported in python2.subprocess
-                popen = subprocess.Popen(args, **kwargs)
-                popen.communicate()
+            popen = subprocess.Popen(args, start_new_session=True, **kwargs)
+            popen.communicate(timeout=timeout)
             if popen.returncode != 0:
                 raise OSError()
+        except subprocess.TimeoutExpired as e:
+            print(f'Timeout for {args[0]} ({timeout}s) expired',
+                  file=sys.stderr)
+            print('Terminating the whole process group...', file=sys.stderr)
+            os.killpg(os.getpgid(popen.pid), signal.SIGTERM)
+            error = (type(e), e, sys.exc_info()[2])
         except Exception as e:
             error = (type(e), e, sys.exc_info()[2])
     finally:
@@ -54,9 +45,9 @@ def system(*args, **kwargs):
             if 'cwd' in kwargs:
                 txt = '%s\nFrom directory: %s' % (txt, kwargs['cwd'])
             if error[1].args:
-                txt += '\n%s' % error[1].args[0]
+                txt += f'\n{error[1].args[0]}'
             error = (error[0], error[0](txt, *error[1].args[1:]), error[2])
-            six.reraise(*error)
+            raise error[0](txt, *error[1].args[1:]) from error[1]
 
 
 def system_output_on_error(*args, **kwargs):
@@ -69,22 +60,14 @@ def system_output_on_error(*args, **kwargs):
     if echo:
         print(' '.join([str(x) for x in args]))
     try:
-        if subprocess32 is not None:
-            # !!! Redirecting STDERR to STDOUT is a burden on Windows OS and
-            # lead to enormous processing times using "wine" (x80) ... I do not
-            # know why.
-            # The issue can be reproduced using commands:
-            # time python -c 'import subprocess;print subprocess.check_output(["winepath", "-u", "c:\\"]).strip()'
-            # time python -c 'import subprocess;print subprocess.check_output(["winepath", "-u", "c:\\"], stderr=subprocess.STDOUT).strip()'
-            output = subprocess32.check_output(*args, stderr=subprocess.STDOUT,
-                                               **kwargs)
-        else:
-            if 'timeout' in kwargs:
-                # timeout not supported in python2.subprocess
-                kwargs = dict(kwargs)
-                del kwargs['timeout']
-            output = subprocess.check_output(*args, stderr=subprocess.STDOUT,
-                                             **kwargs)
+        # !!! Redirecting STDERR to STDOUT is a burden on Windows OS and
+        # lead to enormous processing times using "wine" (x80) ... I do not
+        # know why.
+        # The issue can be reproduced using commands:
+        # time python -c 'import subprocess;print subprocess.check_output(["winepath", "-u", "c:\\"]).strip()'
+        # time python -c 'import subprocess;print subprocess.check_output(["winepath", "-u", "c:\\"], stderr=subprocess.STDOUT).strip()'
+        output = subprocess.check_output(*args, stderr=subprocess.STDOUT,
+                                         **kwargs)
     except subprocess.CalledProcessError as e:
         print('-- failed command: --')
         print('-- command:', args)
